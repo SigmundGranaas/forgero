9c8644ab4bf759fdc78bcfd263468da147f24c6c
Sigmund Granaas <sigmundsgranaas@gmail.com>
2021-10-17 02:27:45 +0200

    Added simple methods for creating ForgeroToolParts from Nbt's and Items. *Unfinished*

--- src/main/java/com/sigmundgranaas/forgero/item/forgerotool/toolpart/ForgeroToolPartCreator.java
+++ src/main/java/com/sigmundgranaas/forgero/item/forgerotool/toolpart/ForgeroToolPartCreator.java
@@ -1,30 +1,51 @@
 package com.sigmundgranaas.forgero.item.forgerotool.toolpart;
 
+import com.sigmundgranaas.forgero.item.forgerotool.Modifier.EmptyModifier;
+import com.sigmundgranaas.forgero.item.forgerotool.Modifier.ForgeroModifier;
+import com.sigmundgranaas.forgero.item.forgerotool.material.ForgeroMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ToolMaterial;
 import net.minecraft.nbt.NbtCompound;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
 
 public class ForgeroToolPartCreator {
-    public static AbstractToolPart createToolPart(NbtCompound nbtCompound) {
-        return null;
-    }
+    public static Optional<ForgeroToolPart> createToolPart(NbtCompound nbtCompound) {
+        String primaryMaterialIdentifier = nbtCompound.getString(ForgeroToolPart.PRIMARY_MATERIAL_IDENTIFIER);
 
-    public static AbstractToolPart createToolPart(ItemStack forgeroToolPart) {
-        return null;
-    }
-
-    public static AbstractToolPart createToolPart(ForgeroToolPartItem forgeroToolPart) {
-        switch (forgeroToolPart.getToolPartType()) {
-            case HANDLE -> new ForgeroToolHandle(forgeroToolPart.getMaterial());
+        @Nullable
+        ToolMaterial primaryMaterial = ForgeroMaterial.getMaterialMap().get(primaryMaterialIdentifier);
+        if (primaryMaterial == null){
+            return Optional.empty();
         }
-        return;
+
+        @Nullable
+        ToolMaterial secondaryMaterial = ForgeroMaterial.getMaterialMap().get(nbtCompound.getString(ForgeroToolPart.SECONDARY_MATERIAL_IDENTIFIER));
+        if(secondaryMaterial == null){
+            secondaryMaterial = ForgeroMaterial.EMPTY_MATERIAL;
+        }
+
+        ForgeroModifier modifier = new EmptyModifier();
+
+
+        return Optional.of(new ForgeroToolPart(primaryMaterial, secondaryMaterial, modifier));
     }
 
-    public static AbstractToolPart createToolPart(Item forgeroToolPart) {
+    public static ForgeroToolPart createToolPart(ItemStack forgeroToolPart) {
         return null;
     }
 
-    private AbstractToolPart createBasicToolPart() {
+    public static ForgeroToolPart createToolPart(ForgeroToolPartItem forgeroToolPart) {
+        return new ForgeroToolPart(forgeroToolPart.getMaterial(), ForgeroMaterial.EMPTY_MATERIAL, new EmptyModifier());
+    }
+
+    public static ForgeroToolPart createToolPart(Item forgeroToolPart) {
+        return null;
+    }
+
+    private ForgeroToolPart createBasicToolPart() {
         return null;
     }
 }
