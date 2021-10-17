931b7ca3b4b52222e3483a6cdcf9c70885e867a1
Sigmund Granaas <sigmundsgranaas@gmail.com>
2021-10-17 02:24:30 +0200

    Renamed AbstractToolPart to ToolPart because it now handles all logic for ToolParts

--- src/main/java/com/sigmundgranaas/forgero/item/forgerotool/toolpart/ForgeroToolPart.java
+++ src/main/java/com/sigmundgranaas/forgero/item/forgerotool/toolpart/ForgeroToolPart.java
@@ -1,24 +1,25 @@
 package com.sigmundgranaas.forgero.item.forgerotool.toolpart;
 
 import com.sigmundgranaas.forgero.item.forgerotool.Modifier.ForgeroModifier;
-import com.sigmundgranaas.forgero.item.forgerotool.Modifier.ModifierConstants;
-import com.sigmundgranaas.forgero.item.forgerotool.material.ForgeroMaterial;
-import net.minecraft.nbt.NbtCompound;
+import net.minecraft.item.ToolMaterial;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
+public class ForgeroToolPart {
+    public static final String PRIMARY_MATERIAL_IDENTIFIER = "PrimaryMaterial";
+    public static final String SECONDARY_MATERIAL_IDENTIFIER = "SecondaryMaterial";
+    public static final String MODIFIER_IDENTIFIER = "ForgeroModifier";
+    public static final String FORGERO_TOOL_PART_IDENTIFIER = "ForgeroToolPart";
+    public static final String FORGERO_TOOL_PART_TYPE_IDENTIFIER = "ForgeroToolPartType";
+    private final ToolMaterial primaryMaterial;
+    @Nullable
+    private final ToolMaterial secondaryMaterial;
+    @Nullable
+    private final ForgeroModifier modifier;
 
-public interface ForgeroToolPart {
-    ForgeroModifier getToolPartModifier(ArrayList<ModifierConstants> possibleModifiers);
-
-    ArrayList<ModifierConstants> getPossibleModifiers();
-
-    public NbtCompound getToolPartNbt();
-
-    public ForgeroMaterial getPrimaryMaterial();
-
-    public ForgeroMaterial getSecondaryMaterial();
-
-    public NbtCompound createToolPartNbt();
-
-    public float calculateModifiers();
+    public ForgeroToolPart(@NotNull ToolMaterial primaryMaterial, @NotNull ToolMaterial secondaryMaterial, @NotNull ForgeroModifier modifier) {
+        this.primaryMaterial = primaryMaterial;
+        this.secondaryMaterial = secondaryMaterial;
+        this.modifier = modifier;
+    }
 }
