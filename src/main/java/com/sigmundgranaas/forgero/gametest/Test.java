2e989ae97dd73ed4f48683b78d6c9adfb95d2f57
Sigmund Granaas <sigmundsgranaas@gmail.com>
2021-12-19 18:02:10 +0100

    * Fixed failing gametest
    Signed-off-by: Sigmund Granaas <sigmundsgranaas@gmail.com>

--- src/main/java/com/sigmundgranaas/forgero/gametest/Test.java
+++ src/main/java/com/sigmundgranaas/forgero/gametest/Test.java
@@ -49,7 +49,7 @@ public class Test implements FabricGameTest {
         context.setBlockState(0, 2, 0, Blocks.DIAMOND_BLOCK);
 
         context.addInstantFinalTask(() ->
-                context.checkBlock(new BlockPos(0, 1, 0), (block) -> block == Blocks.DIAMOND_BLOCK, "Expect block to be diamond")
+                context.checkBlock(new BlockPos(0, 1, 0), (block) -> block != Blocks.DIAMOND_BLOCK, "Expect block to be diamond")
         );
     }
 }
