2ea585aaa87da1c3d2d4bac6fdf3b10a1e30c792
Sigmund Granaas <sigmundsgranaas@gmail.com>
2022-02-18 14:03:46 +0100

    Updated to 1.18.1
    Signed-off-by: Sigmund Granaas <sigmundsgranaas@gmail.com>

--- src/test/java/com/sigmundgranaas/forgero/recipe/implementation/RecipeCreatorImplTest.java
+++ src/test/java/com/sigmundgranaas/forgero/recipe/implementation/RecipeCreatorImplTest.java
@@ -1,20 +1,8 @@
 package com.sigmundgranaas.forgero.recipe.implementation;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import org.junit.jupiter.api.Test;
 
 class RecipeCreatorImplTest {
-
-    private String prettyJson(String uglyJson) {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        JsonParser jp = new JsonParser();
-        JsonElement je = jp.parse(uglyJson);
-        return gson.toJson(je);
-    }
-
     @Test
     void ListOfRecipesCreated() {
         //Assertions.assertTrue(RecipeCreator.INSTANCE.createRecipes().size() > 10);
