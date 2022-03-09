59280ad604bac61a7832dcecbc322b3e9fcbd41f
Sigmund Granaas <sigmundsgranaas@gmail.com>
2022-03-09 02:04:50 +0100

    Created some baseline tests
    Signed-off-by: Sigmund Granaas <sigmundsgranaas@gmail.com>

--- src/test/java/com/sigmundgranaas/forgero/core/properties/WoodenToolPropertyTest.java
+++ src/test/java/com/sigmundgranaas/forgero/core/properties/WoodenToolPropertyTest.java
@@ -12,13 +12,13 @@ import com.sigmundgranaas.forgero.core.toolpart.head.ToolPartHead;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static com.sigmundgranaas.forgero.core.properties.ToolPropertyTest.*;
+import static com.sigmundgranaas.forgero.core.properties.ToolPropertyTest.OAK_PRIMARY;
 
-public class IronToolPropertyTest {
+public class WoodenToolPropertyTest {
 
     @Test
     void testToolDurability() {
-        ToolPartBuilder headBuilder = new ToolPartHeadBuilder(IRON_PRIMARY.get(), ForgeroToolTypes.PICKAXE);
+        ToolPartBuilder headBuilder = new ToolPartHeadBuilder(OAK_PRIMARY.get(), ForgeroToolTypes.PICKAXE);
         //headBuilder.setSecondary(DIAMOND_SECONDARY.get());
 
         ToolPartBuilder handleBuilder = new ToolPartHandleBuilder(OAK_PRIMARY.get());
@@ -26,25 +26,12 @@ public class IronToolPropertyTest {
 
         ForgeroTool exampleTool = ForgeroToolFactory.INSTANCE.createForgeroTool((ToolPartHead) headBuilder.createToolPart(), (ToolPartHandle) handleBuilder.createToolPart());
 
-        Assertions.assertTrue(exampleTool.getPropertyStream().applyAttribute(Target.createEmptyTarget(), AttributeType.DURABILITY) > 250);
-    }
-
-    @Test
-    void testToolDurabilityWithSecondary() {
-        ToolPartBuilder headBuilder = new ToolPartHeadBuilder(IRON_PRIMARY.get(), ForgeroToolTypes.PICKAXE);
-        headBuilder.setSecondary(DIAMOND_SECONDARY.get());
-
-        ToolPartBuilder handleBuilder = new ToolPartHandleBuilder(OAK_PRIMARY.get());
-        handleBuilder.setSecondary(DIAMOND_SECONDARY.get());
-
-        ForgeroTool exampleTool = ForgeroToolFactory.INSTANCE.createForgeroTool((ToolPartHead) headBuilder.createToolPart(), (ToolPartHandle) handleBuilder.createToolPart());
-
-        Assertions.assertEquals(1200, exampleTool.getPropertyStream().applyAttribute(Target.createEmptyTarget(), AttributeType.DURABILITY), 200);
+        Assertions.assertEquals(30, exampleTool.getPropertyStream().applyAttribute(Target.createEmptyTarget(), AttributeType.DURABILITY), 20);
     }
 
     @Test
     void testToolMiningLevel() {
-        ToolPartBuilder headBuilder = new ToolPartHeadBuilder(IRON_PRIMARY.get(), ForgeroToolTypes.AXE);
+        ToolPartBuilder headBuilder = new ToolPartHeadBuilder(OAK_PRIMARY.get(), ForgeroToolTypes.AXE);
         //headBuilder.setSecondary(DIAMOND_SECONDARY.get());
 
         ToolPartBuilder handleBuilder = new ToolPartHandleBuilder(OAK_PRIMARY.get());
@@ -52,12 +39,12 @@ public class IronToolPropertyTest {
 
         ForgeroTool exampleTool = ForgeroToolFactory.INSTANCE.createForgeroTool((ToolPartHead) headBuilder.createToolPart(), (ToolPartHandle) handleBuilder.createToolPart());
 
-        Assertions.assertEquals(2, exampleTool.getPropertyStream().applyAttribute(Target.createEmptyTarget(), AttributeType.MINING_LEVEL));
+        Assertions.assertEquals(0, exampleTool.getPropertyStream().applyAttribute(Target.createEmptyTarget(), AttributeType.MINING_LEVEL));
     }
 
     @Test
     void testToolMiningSpeed() {
-        ToolPartBuilder headBuilder = new ToolPartHeadBuilder(IRON_PRIMARY.get(), ForgeroToolTypes.PICKAXE);
+        ToolPartBuilder headBuilder = new ToolPartHeadBuilder(OAK_PRIMARY.get(), ForgeroToolTypes.PICKAXE);
         //headBuilder.setSecondary(DIAMOND_SECONDARY.get());
 
         ToolPartBuilder handleBuilder = new ToolPartHandleBuilder(OAK_PRIMARY.get());
@@ -65,6 +52,6 @@ public class IronToolPropertyTest {
 
         ForgeroTool exampleTool = ForgeroToolFactory.INSTANCE.createForgeroTool((ToolPartHead) headBuilder.createToolPart(), (ToolPartHandle) handleBuilder.createToolPart());
 
-        Assertions.assertEquals(7, exampleTool.getPropertyStream().applyAttribute(Target.createEmptyTarget(), AttributeType.MINING_SPEED), 1);
+        Assertions.assertEquals(2, exampleTool.getPropertyStream().applyAttribute(Target.createEmptyTarget(), AttributeType.MINING_SPEED), 1);
     }
 }
