adea0677d26d9966afdee2f81321033d5f8ce093
Sigmund Granaas <sigmundsgranaas@gmail.com>
2022-04-17 14:45:58 +0200

    Reduced tool part size when rendered on the ground

--- src/main/java/com/sigmundgranaas/forgero/mixins/ItemRenderContextMixin.java
+++ src/main/java/com/sigmundgranaas/forgero/mixins/ItemRenderContextMixin.java
@@ -1,6 +1,7 @@
 package com.sigmundgranaas.forgero.mixins;
 
 import com.sigmundgranaas.forgero.item.ForgeroToolItem;
+import com.sigmundgranaas.forgero.item.ToolPartItem;
 import net.fabricmc.fabric.api.renderer.v1.model.FabricBakedModel;
 import net.fabricmc.fabric.impl.client.indigo.renderer.render.ItemRenderContext;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -24,7 +25,7 @@ public class ItemRenderContextMixin {
 
     @Inject(method = "renderModel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;translate(DDD)V"))
     public void renderModel(ItemStack itemStack, ModelTransformation.Mode transformMode, boolean invert, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int lightmap, int overlay, FabricBakedModel model, ItemRenderContext.VanillaQuadHandler vanillaHandler, CallbackInfo ci) {
-        if (itemStack.getItem() instanceof ForgeroToolItem && this.transformMode == ModelTransformation.Mode.GROUND) {
+        if ((itemStack.getItem() instanceof ForgeroToolItem || itemStack.getItem() instanceof ToolPartItem) && this.transformMode == ModelTransformation.Mode.GROUND) {
             this.matrixStack.scale(0.5f, 0.5f, 0.5f);
         }
     }
