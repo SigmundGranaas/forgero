fb001300c1f394b63cf8d92340d2b7ba5d82fd5e
Sigmund Granaas <sigmundsgranaas@gmail.com>
2022-05-01 13:35:30 +0200

    Added randomization to model faces to eliminate z-fighting issues
    Signed-off-by: Sigmund Granaas <sigmundsgranaas@gmail.com>

--- src/main/java/com/sigmundgranaas/forgero/client/forgerotool/model/toolpart/BakedToolPartModel.java
+++ src/main/java/com/sigmundgranaas/forgero/client/forgerotool/model/toolpart/BakedToolPartModel.java
@@ -1,18 +1,11 @@
 package com.sigmundgranaas.forgero.client.forgerotool.model.toolpart;
 
 import com.sigmundgranaas.forgero.client.forgerotool.model.implementation.ToolPartModelBuilderImpl;
-import net.fabricmc.fabric.api.renderer.v1.Renderer;
-import net.fabricmc.fabric.api.renderer.v1.RendererAccess;
-import net.fabricmc.fabric.api.renderer.v1.mesh.MeshBuilder;
-import net.fabricmc.fabric.api.renderer.v1.mesh.MutableQuadView;
-import net.fabricmc.fabric.api.renderer.v1.mesh.QuadEmitter;
 import net.fabricmc.fabric.api.renderer.v1.model.FabricBakedModel;
 import net.fabricmc.fabric.api.renderer.v1.render.RenderContext;
 import net.minecraft.block.BlockState;
-import net.minecraft.client.render.model.BasicBakedModel;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockRenderView;
 
 import java.util.Random;
@@ -44,26 +37,5 @@ public class BakedToolPartModel implements FabricBakedModel {
         this.model.emitItemQuads(null, null, context);
         this.secondaryMaterial.emitItemQuads(null, null, context);
         this.gem.emitItemQuads(null, null, context);
-        if (model instanceof BasicBakedModel basic) {
-            var quads = basic.getQuads(null, null, null);
-            boolean check = false;
-            Renderer renderer = RendererAccess.INSTANCE.getRenderer();
-            MeshBuilder builder = renderer.meshBuilder();
-            QuadEmitter emitter = builder.getEmitter();
-            for (Direction direction : Direction.values()) {
-                int spriteIdx = direction == Direction.UP || direction == Direction.DOWN ? 1 : 0;
-                // Add a new face to the mesh
-                emitter.square(direction, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f);
-                // Set the sprite of the face, must be called after .square()
-                // We haven't specified any UV coordinates, so we want to use the whole texture. BAKE_LOCK_UV does exactly that.
-                emitter.spriteBake(0, basic.getParticleSprite(), MutableQuadView.BAKE_LOCK_UV);
-                // Enable texture usage
-                emitter.spriteColor(0, -1, -1, -1, -1);
-                // Add the quad to the mesh
-                emitter.emit();
-            }
-
-            context.meshConsumer().accept(builder.build());
-        }
     }
 }
