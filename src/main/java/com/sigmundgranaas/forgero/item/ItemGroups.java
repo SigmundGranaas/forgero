d73751eefda07b7bd1ec7139054e06613b7e72d3
Sigmund Granaas <sigmundsgranaas@gmail.com>
2022-06-08 15:26:40 +0200

    Updated icon for Forgero Group tab
    Signed-off-by: Sigmund Granaas <sigmundsgranaas@gmail.com>

--- src/main/java/com/sigmundgranaas/forgero/item/ItemGroups.java
+++ src/main/java/com/sigmundgranaas/forgero/item/ItemGroups.java
@@ -1,6 +1,15 @@
 package com.sigmundgranaas.forgero.item;
 
 import com.sigmundgranaas.forgero.ForgeroInitializer;
+import com.sigmundgranaas.forgero.core.ForgeroRegistry;
+import com.sigmundgranaas.forgero.core.tool.ForgeroToolWithBinding;
+import com.sigmundgranaas.forgero.core.toolpart.ForgeroToolPart;
+import com.sigmundgranaas.forgero.core.toolpart.binding.ToolPartBinding;
+import com.sigmundgranaas.forgero.core.toolpart.factory.ToolPartBindingBuilder;
+import com.sigmundgranaas.forgero.core.toolpart.factory.ToolPartHandleBuilder;
+import com.sigmundgranaas.forgero.core.toolpart.factory.ToolPartHeadBuilder;
+import com.sigmundgranaas.forgero.core.toolpart.handle.ToolPartHandle;
+import com.sigmundgranaas.forgero.core.toolpart.head.ToolPartHead;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -14,6 +23,27 @@ public class ItemGroups {
 
     public static final ItemGroup FORGERO_TOOL_PARTS = FabricItemGroupBuilder.create(
                     new Identifier(ForgeroInitializer.MOD_NAMESPACE, FORGERO_GROUP))
-            .icon(() -> new ItemStack(Registry.ITEM.get(new Identifier(ForgeroInitializer.MOD_NAMESPACE, "oak" + ELEMENT_SEPARATOR + "handle"))))
+            .icon(ItemGroups::createForgeroToolIcon)
             .build();
+
+    private static ItemStack createForgeroToolIcon() {
+        ForgeroToolPart head = new ToolPartHeadBuilder(ForgeroRegistry.TOOL_PART.getHead("iron-pickaxehead").get())
+                .setSecondary(ForgeroRegistry.MATERIAL.getSecondaryMaterial("netherite").get())
+                .setGem(ForgeroRegistry.GEM.getResource("lapis-gem").get())
+                .createToolPart();
+
+        ForgeroToolPart handle = new ToolPartHandleBuilder(ForgeroRegistry.TOOL_PART.getHandle("spruce-handle").get())
+                .setSecondary(ForgeroRegistry.MATERIAL.getSecondaryMaterial("rabbit_hide").get())
+                .setGem(ForgeroRegistry.GEM.getResource("diamond-gem").get())
+                .createToolPart();
+
+        ForgeroToolPart binding = new ToolPartBindingBuilder(ForgeroRegistry.TOOL_PART.getBinding("netherite-binding").get())
+                .setSecondary(ForgeroRegistry.MATERIAL.getSecondaryMaterial("leather").get())
+                .createToolPart();
+
+        var tool = new ForgeroToolWithBinding((ToolPartHead) head, (ToolPartHandle) handle, (ToolPartBinding) binding);
+        var stack = new ItemStack(Registry.ITEM.get(new Identifier(ForgeroInitializer.MOD_NAMESPACE, "iron" + ELEMENT_SEPARATOR + "pickaxe")));
+        stack.getOrCreateNbt().put(NBTFactory.FORGERO_TOOL_NBT_IDENTIFIER, NBTFactory.INSTANCE.createNBTFromTool(tool));
+        return stack;
+    }
 }
