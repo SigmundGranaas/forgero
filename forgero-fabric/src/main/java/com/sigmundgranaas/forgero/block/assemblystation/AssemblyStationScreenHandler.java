c32fcacfd3c04b81488c1da95919fd7ae3562b45
Sigmund Granaas <sigmundsgranaas@gmail.com>
2022-12-03 21:45:58 +0100

    Better stack handling for the assembly table

--- forgero-fabric/src/main/java/com/sigmundgranaas/forgero/block/assemblystation/AssemblyStationScreenHandler.java
+++ forgero-fabric/src/main/java/com/sigmundgranaas/forgero/block/assemblystation/AssemblyStationScreenHandler.java
@@ -20,6 +20,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -66,13 +67,13 @@ public class AssemblyStationScreenHandler extends ScreenHandler {
         super(AssemblyStationScreenHandler.ASSEMBLY_STATION_SCREEN_HANDLER, syncId);
         this.player = playerInventory.player;
         this.context = context;
-        this.inventory = new SimpleInventory(9);
+        this.inventory = new SimpleInventory(10);
         inventory.addListener(this::onContentChanged);
         //some inventories do custom logic when a player opens it.
         inventory.onOpen(playerInventory.player);
         SimpleInventory compositeInventory = new SimpleInventory(1);
         compositeInventory.addListener(this::onCompositeSlotChanged);
-        this.compositeSlot = new CompositeSlot(compositeInventory, 0, 34, 34);
+        this.compositeSlot = new CompositeSlot(compositeInventory, 0, 34, 34, inventory);
 
         //This will place the slot in the correct locations for a 3x3 Grid. The slots exist on both server and client!
         //This will not render the background of the slots however, this is the Screens job
@@ -139,6 +140,7 @@ public class AssemblyStationScreenHandler extends ScreenHandler {
         return newStack;
     }
 
+
     @Override
     public boolean canInsertIntoSlot(ItemStack stack, Slot slot) {
         return true;
@@ -168,10 +170,10 @@ public class AssemblyStationScreenHandler extends ScreenHandler {
             if (!world.isClient && compositeSlot.isRemovable()) {
                 ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity) player;
                 var output = craftInventory(world);
-                if (output.isPresent() && compositeSlot.isRemovable()) {
+                if (output.isPresent() && compositeSlot.isRemovable() && StateConverter.of(output.get()).filter(Composite.class::isInstance).isPresent()) {
                     compositeSlot.setStack(output.get());
                     serverPlayerEntity.networkHandler.sendPacket(new ScreenHandlerSlotUpdateS2CPacket(this.syncId, this.nextRevision(), 0, output.get()));
-                } else if (!compositeSlot.isEmpty() && output.isEmpty()) {
+                } else if (!compositeSlot.isEmpty() && output.isEmpty() && compositeSlot.doneConstructing && !isDeconstructedInventory(compositeSlot.composite)) {
                     compositeSlot.removeCompositeIngredient();
                     serverPlayerEntity.networkHandler.sendPacket(new ScreenHandlerSlotUpdateS2CPacket(this.syncId, this.nextRevision(), 0, ItemStack.EMPTY));
                 }
@@ -213,10 +215,19 @@ public class AssemblyStationScreenHandler extends ScreenHandler {
         });
     }
 
+    private List<ItemStack> deconstructedItems(Composite composite) {
+        return composite.disassemble().stream().map(StateConverter::of).toList();
+    }
+
+    private boolean isDeconstructedInventory(Composite composite) {
+        var deconstructed = deconstructedItems(composite);
+        return IntStream.range(0, deconstructed.size()).allMatch(index -> deconstructed.get(index).getItem() == inventory.getStack(index).getItem());
+    }
+
     private Optional<ItemStack> craftInventory(World world) {
         if (!world.isClient) {
             var craftingInventory = new CraftingInventory(dummyHandler, 3, 3);
-            IntStream.range(0, 9).forEach(index -> craftingInventory.setStack(index, this.inventory.getStack(index)));
+            IntStream.range(0, 8).forEach(index -> craftingInventory.setStack(index, this.inventory.getStack(index)));
             return world.getRecipeManager().getFirstMatch(RecipeType.CRAFTING, craftingInventory, world).map(recipe -> recipe.craft(craftingInventory));
         }
         return Optional.empty();
@@ -227,19 +238,21 @@ public class AssemblyStationScreenHandler extends ScreenHandler {
         this.context.run((world, pos) -> {
             if (!world.isClient) {
                 compositeSlot.removeCompositeIngredient();
-                IntStream.range(0, 9).forEach(index -> inventory.getStack(index).decrement(1));
+                IntStream.range(0, 8).filter(index -> !inventory.isEmpty()).forEach(index -> inventory.getStack(index).decrement(1));
             }
         });
     }
 
     private static class CompositeSlot extends Slot {
+        private final Inventory craftingInventory;
         private Composite composite;
         private boolean isConstructed = false;
         private boolean doneConstructing = false;
 
 
-        public CompositeSlot(Inventory inventory, int index, int x, int y) {
+        public CompositeSlot(Inventory inventory, int index, int x, int y, Inventory craftingInventory) {
             super(inventory, index, x, y);
+            this.craftingInventory = craftingInventory;
         }
 
         Optional<Composite> getComposite() {
@@ -268,20 +281,23 @@ public class AssemblyStationScreenHandler extends ScreenHandler {
             this.composite = null;
             this.isConstructed = false;
             this.doneConstructing = true;
-            this.inventory.getStack(0).decrement(1);
+            if (!this.inventory.getStack(0).isEmpty()) {
+                this.inventory.getStack(0).decrement(1);
+            }
         }
 
         @Override
         public boolean canInsert(ItemStack stack) {
-            return StateConverter.of(stack).filter(Composite.class::isInstance).isPresent();
+            return StateConverter.of(stack).filter(Composite.class::isInstance).isPresent() && craftingInventory.isEmpty();
         }
+
 
         public void doneConstructing() {
             this.doneConstructing = true;
         }
 
         public boolean isRemovable() {
-            if (inventory.isEmpty()) {
+            if (craftingInventory.isEmpty()) {
                 return true;
             }
             return doneConstructing || !isConstructed;
