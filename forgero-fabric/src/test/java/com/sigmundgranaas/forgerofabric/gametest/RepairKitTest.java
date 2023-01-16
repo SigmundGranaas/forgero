0251aef27f249486c0e926c71c40ce820676f38e
Sigmund Granaas <sigmundsgranaas@gmail.com>
2023-01-16 09:20:40 +0100

    Fixed repair kit recipes

--- forgero-fabric/src/test/java/com/sigmundgranaas/forgerofabric/gametest/RepairKitTest.java
+++ forgero-fabric/src/test/java/com/sigmundgranaas/forgerofabric/gametest/RepairKitTest.java
@@ -3,11 +3,11 @@ package com.sigmundgranaas.forgerofabric.gametest;
 import com.sigmundgranaas.forgerofabric.testutil.RecipeTester;
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
 import net.minecraft.test.GameTest;
 import net.minecraft.test.GameTestException;
 import net.minecraft.test.TestContext;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 import java.util.function.Supplier;
 
@@ -16,7 +16,7 @@ public class RepairKitTest {
 
     @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE, batchId = "recipe_test", required = true)
     public void testRepairDiamondPickaxe(TestContext context) {
-        var tool = new ItemStack(Registry.ITEM.get(new Identifier("forgero:diamond-pickaxe")));
+        var tool = new ItemStack(Registries.ITEM.get(new Identifier("forgero:diamond-pickaxe")));
         tool.setDamage(100);
         var test = RecipeTester.repairKit("diamond_repair_kit", tool, "forgero:diamond-pickaxe",context);
         var result = test.craft();
@@ -30,7 +30,7 @@ public class RepairKitTest {
 
     @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE, batchId = "recipe_test", required = true)
     public void testRepairDiamondPickaxeWithFullDurability(TestContext context) {
-        var tool = new ItemStack(Registry.ITEM.get(new Identifier("forgero:diamond-pickaxe")));
+        var tool = new ItemStack(Registries.ITEM.get(new Identifier("forgero:diamond-pickaxe")));
         var test = RecipeTester.repairKit("diamond_repair_kit", tool, "forgero:diamond-pickaxe",context);
         var result = test.craft();
         if(result.isPresent()){
