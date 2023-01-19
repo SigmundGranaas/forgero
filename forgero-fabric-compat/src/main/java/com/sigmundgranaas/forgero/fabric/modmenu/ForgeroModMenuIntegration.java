f12e79c3c35c0ccad142d7f54e325d407e2e17f0
Sigmund Granaas <sigmundsgranaas@gmail.com>
2023-01-19 03:11:25 +0100

    Moved from Screen to GameOptionsScreen

--- forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/modmenu/ForgeroModMenuIntegration.java
+++ forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/modmenu/ForgeroModMenuIntegration.java
@@ -2,13 +2,10 @@ package com.sigmundgranaas.forgero.fabric.modmenu;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import net.minecraft.text.Text;
 
 public class ForgeroModMenuIntegration implements ModMenuApi {
 	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		return parent -> {
-			return new ForgeroConfigScreen(Text.translatable("screen.forgero.config.title"));
-		};
+		return ForgeroConfigScreen::new;
 	}
 }
