833102407c0fd7b7617d9a973b4e1a4aac21f445
Darion Spaargaren <djspaargaren@outlook.com>
2023-01-23 00:43:54 +0100

    fix: Fix crash when creating a new configuration file

--- forgero-core/src/main/java/com/sigmundgranaas/forgero/core/configuration/ForgeroConfigurationData.java
+++ forgero-core/src/main/java/com/sigmundgranaas/forgero/core/configuration/ForgeroConfigurationData.java
@@ -1,10 +1,5 @@
 package com.sigmundgranaas.forgero.core.configuration;
 
-import com.google.gson.*;
-
-import java.lang.reflect.Type;
-import java.util.List;
-
 public interface ForgeroConfigurationData {
 	default void setByKey(String key, Object value) {
 		try {
@@ -20,40 +15,6 @@ public interface ForgeroConfigurationData {
 		} catch (NoSuchFieldException | IllegalAccessException e) {
 			e.printStackTrace();
 			return null;
-		}
-	}
-
-	class Deserializer implements JsonDeserializer<ForgeroConfiguration> {
-		@Override
-		public ForgeroConfiguration deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-			JsonObject data = json.getAsJsonObject();
-			var forgeroConfiguration = new ForgeroConfiguration();
-
-			data.keySet().forEach(key ->
-			{
-				JsonElement value = data.get(key);
-
-				if (value.getClass() == JsonArray.class) {
-					var valueAsList = new Gson().fromJson(value.toString(), List.class);
-
-					forgeroConfiguration.setByKey(key, valueAsList);
-					return;
-				}
-
-				if (forgeroConfiguration.getByKey(key).getClass() == Boolean.class) {
-					var valueAsBoolean = value.getAsBoolean();
-
-					forgeroConfiguration.setByKey(key, valueAsBoolean);
-					return;
-				}
-
-				forgeroConfiguration.setByKey(key, value);
-
-				// TODO: Iterate over nested objects
-//				if (value instanceof JsonObject)
-			});
-
-			return forgeroConfiguration;
 		}
 	}
 }
--- forgero-core/src/main/java/com/sigmundgranaas/forgero/core/configuration/ForgeroConfigurationLoader.java
+++ forgero-core/src/main/java/com/sigmundgranaas/forgero/core/configuration/ForgeroConfigurationLoader.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonReader;
 import com.sigmundgranaas.forgero.core.Forgero;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -18,25 +19,19 @@ public class ForgeroConfigurationLoader {
 	public static Path configurationFilePath = Path.of(MessageFormat.format("./{0}/{1}", configurationFolderName, configurationFileName));
 
 	public static ForgeroConfiguration configuration;
-	public static ForgeroConfiguration.Deserializer deserializer;
 
 	public static ForgeroConfiguration load() {
-		if (deserializer == null) deserializer = new ForgeroConfigurationData.Deserializer();
-
 		if (!Files.exists(configurationFilePath)) {
 			configuration = createConfigurationFile();
 			return configuration;
 		}
 
 		try (InputStream stream = Files.newInputStream(configurationFilePath)) {
-			GsonBuilder gsonBuilder = new GsonBuilder();
-			gsonBuilder.registerTypeAdapter(ForgeroConfiguration.class, deserializer);
-
-			Gson configurationGson = gsonBuilder.create();
-			configuration = configurationGson.fromJson(new JsonReader(new BufferedReader(new InputStreamReader(stream))), ForgeroConfiguration.class);
+			var gson = createGson();
+			configuration = gson.fromJson(new JsonReader(new BufferedReader(new InputStreamReader(stream))), ForgeroConfiguration.class);
 
 			if (configuration.resourceLogging) {
-				Forgero.LOGGER.info("(Re)loaded Forgero configuration, located at: {}", configurationFilePath);
+				Forgero.LOGGER.info("Loaded Forgero configuration, located at: {}", configurationFilePath);
 			}
 
 			return configuration;
@@ -50,10 +45,8 @@ public class ForgeroConfigurationLoader {
 		}
 	}
 
-	private static Gson createGson() {
+	private static @NotNull Gson createGson() {
 		GsonBuilder gsonBuilder = new GsonBuilder();
-		gsonBuilder.registerTypeAdapter(ForgeroConfiguration.class, ForgeroConfigurationLoader.deserializer);
-
 		return gsonBuilder.setPrettyPrinting().create();
 	}
 
@@ -66,17 +59,15 @@ public class ForgeroConfigurationLoader {
 		}
 
 		try (FileWriter writer = new FileWriter(configurationFilePath.toString())) {
-			var forgeroConfiguration = new ForgeroConfiguration();
-			var json = createGson().toJson(forgeroConfiguration);
+			var gson = createGson();
+			var json = gson.toJson(defaultConfiguration);
 			writer.write(json);
 
-			if (configuration.resourceLogging) {
-				Forgero.LOGGER.info("(Re)created Forgero configuration file, located at {}", configurationFilePath);
-			}
+			Forgero.LOGGER.info("Created Forgero configuration file, located at {}", configurationFilePath);
 
-			return forgeroConfiguration;
+			return defaultConfiguration;
 		} catch (IOException e) {
-			Forgero.LOGGER.info("Unable to create Forgero configuration file at {}. See stack trace below:", configurationFilePath);
+			Forgero.LOGGER.warn("Unable to create Forgero configuration file at {}. Loading default configuration. See stack trace below:", configurationFilePath);
 			e.printStackTrace();
 
 			// Worst case scenario, return the default configuration
