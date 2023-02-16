9afc3013e81da209facc7b89da0636ca39a35fa1
holmraven <16858119+holmraven@users.noreply.github.com>
2023-02-16 19:34:36 +0200

    Fix mixin injection failed logging

--- forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/ForgeroCompatInitializer.java
+++ forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/ForgeroCompatInitializer.java
@@ -7,15 +7,31 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 
 public class ForgeroCompatInitializer implements ModInitializer {
+    public static final boolean toolstats;
+    public static final boolean patchouli;
+    public static final boolean modmenu;
+    public static final boolean bettercombat;
+
+    static {
+        toolstats = isModLoaded("toolstats");
+        patchouli = isModLoaded("patchouli");
+        modmenu = isModLoaded("modmenu");
+        bettercombat = isModLoaded("bettercombat");
+    }
+
     @Override
     public void onInitialize() {
-        if (FabricLoader.getInstance().isModLoaded("toolstats")) {
+        if (toolstats) {
             ToolStatTagGenerator.generateTags();
         }
 
-        if (FabricLoader.getInstance().isModLoaded("patchouli")) {
+        if (patchouli) {
             BookDropOnAdvancement.registerBookDrop();
             GuideBookGenerator.registerGuideBookRecipes();
         }
+    }
+
+    public static boolean isModLoaded(String id) {
+        return FabricLoader.getInstance().isModLoaded(id);
     }
 }
