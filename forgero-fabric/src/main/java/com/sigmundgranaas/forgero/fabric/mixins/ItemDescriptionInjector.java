2198cd3e747f60b02b2a8f4a63f19ff5a86b2f17
sigmundgranaas <sigmundsgranaas@gmail.com>
2023-02-16 22:43:00 +0100

    Added new mixin to view stats from non-forgero added items

---
+++ forgero-fabric/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/ItemDescriptionInjector.java
@@ -0,0 +1,35 @@
+package com.sigmundgranaas.forgero.fabric.mixins;
+
+import com.sigmundgranaas.forgero.core.ForgeroStateRegistry;
+import com.sigmundgranaas.forgero.minecraft.common.item.StateItem;
+import com.sigmundgranaas.forgero.minecraft.common.item.tooltip.AttributeWriter;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+@Mixin(Item.class)
+public class ItemDescriptionInjector {
+
+    @SuppressWarnings("DataFlowIssue")
+    @Inject(at = @At("HEAD"), method = "appendTooltip")
+    public void forgero$InjectForgeroAttributes(ItemStack stack, World world, List<Text> tooltip, TooltipContext context, CallbackInfo ci) {
+        Item item = (Item) (Object) this;
+        String id = Registry.ITEM.getId(item).toString();
+        if (!(item instanceof StateItem) && ForgeroStateRegistry.CONTAINER_TO_STATE.containsKey(id)) {
+            ForgeroStateRegistry.STATES
+                    .find(ForgeroStateRegistry.CONTAINER_TO_STATE.get(id))
+                    .map(Supplier::get)
+                    .ifPresent(state -> AttributeWriter.write(state, tooltip, context));
+        }
+    }
+}
