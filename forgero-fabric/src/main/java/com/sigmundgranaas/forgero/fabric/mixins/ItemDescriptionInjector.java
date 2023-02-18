1d3a4cdfcb19ebac9bcf0ed09f72fd6c2667540f
sigmundgranaas <sigmundsgranaas@gmail.com>
2023-02-18 18:16:17 +0100

    Added shift right click trigger

--- forgero-fabric/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/ItemDescriptionInjector.java
+++ forgero-fabric/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/ItemDescriptionInjector.java
@@ -3,10 +3,12 @@ package com.sigmundgranaas.forgero.fabric.mixins;
 import com.sigmundgranaas.forgero.core.ForgeroStateRegistry;
 import com.sigmundgranaas.forgero.minecraft.common.item.StateItem;
 import com.sigmundgranaas.forgero.minecraft.common.tooltip.v2.DefaultWriter;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -26,10 +28,17 @@ public class ItemDescriptionInjector {
         Item item = (Item) (Object) this;
         String id = Registry.ITEM.getId(item).toString();
         if (!(item instanceof StateItem) && ForgeroStateRegistry.CONTAINER_TO_STATE.containsKey(id)) {
-            ForgeroStateRegistry.STATES
-                    .find(ForgeroStateRegistry.CONTAINER_TO_STATE.get(id))
-                    .map(Supplier::get)
-                    .ifPresent(state -> new DefaultWriter(state).write(tooltip, context));
+            if (Screen.hasShiftDown()) {
+                ForgeroStateRegistry.STATES
+                        .find(ForgeroStateRegistry.CONTAINER_TO_STATE.get(id))
+                        .map(Supplier::get)
+                        .ifPresent(state -> new DefaultWriter(state).write(tooltip, context));
+            } else {
+                tooltip.add(Text.translatable("tooltip.forgero.hold").formatted(Formatting.GRAY)
+                        .append(Text.translatable("tooltip.forgero.shift").formatted(Formatting.WHITE))
+                        .append(Text.translatable("tooltip.forgero.to_show_attributes").formatted(Formatting.GRAY))
+                );
+            }
         }
     }
 }
