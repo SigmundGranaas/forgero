dadf702e392ba6b75bba27e42aee22afb323bdc6
Steveplays28 <djspaargaren@outlook.com>
2023-02-20 15:54:02 +0100

    refactor: Use `@Pseudo` annotation instead of a mixin plugin
    Should fix the gametest, since this implementation doesn't depend on ASM.

--- forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/BetterCombatWeaponRegistryMixin.java
+++ forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/BetterCombatWeaponRegistryMixin.java
@@ -9,6 +9,7 @@ import net.bettercombat.logic.WeaponRegistry;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,28 +17,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
 
-
+@Pseudo
 @Mixin(WeaponRegistry.class)
 public abstract class BetterCombatWeaponRegistryMixin {
-    private static final String BETTER_COMPAT_ATTRIBUTE_IDENTIFIER = "better_compat_attribute_container";
-    @Shadow(remap = false)
-    static Map<Identifier, AttributesContainer> containers;
+	private static final String BETTER_COMPAT_ATTRIBUTE_IDENTIFIER = "better_compat_attribute_container";
+	@Shadow(remap = false)
+	static Map<Identifier, AttributesContainer> containers;
 
-    @Inject(method = "getAttributes(Lnet/minecraft/item/ItemStack;)Lnet/bettercombat/api/WeaponAttributes;", at = @At(value = "TAIL"), cancellable = true)
-    private static void injectCustomForgeroAttributes(ItemStack stack, CallbackInfoReturnable<WeaponAttributes> cir) {
-        var state = StateConverter.of(stack);
-        if (state.isPresent() && state.get() instanceof ConstructedTool tool) {
-            var head = tool.getHead();
-            if (head instanceof SchematicBased based) {
-                var better_combat_attributes_id = based.schematic().getCustomValue(BETTER_COMPAT_ATTRIBUTE_IDENTIFIER);
-                if (better_combat_attributes_id.isPresent()) {
-                    var id = new Identifier(better_combat_attributes_id.get().presentableValue());
-                    if (containers.containsKey(id)) {
-                        var container = containers.get(id);
-                        cir.setReturnValue(WeaponRegistry.resolveAttributes(new Identifier(tool.identifier()), container));
-                    }
-                }
-            }
-        }
-    }
+	@Inject(method = "getAttributes(Lnet/minecraft/item/ItemStack;)Lnet/bettercombat/api/WeaponAttributes;", at = @At(value = "TAIL"), cancellable = true)
+	private static void injectCustomForgeroAttributes(ItemStack stack, CallbackInfoReturnable<WeaponAttributes> cir) {
+		var state = StateConverter.of(stack);
+		if (state.isPresent() && state.get() instanceof ConstructedTool tool) {
+			var head = tool.getHead();
+			if (head instanceof SchematicBased based) {
+				var better_combat_attributes_id = based.schematic().getCustomValue(BETTER_COMPAT_ATTRIBUTE_IDENTIFIER);
+				if (better_combat_attributes_id.isPresent()) {
+					var id = new Identifier(better_combat_attributes_id.get().presentableValue());
+					if (containers.containsKey(id)) {
+						var container = containers.get(id);
+						cir.setReturnValue(WeaponRegistry.resolveAttributes(new Identifier(tool.identifier()), container));
+					}
+				}
+			}
+		}
+	}
 }
--- forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/ModMenuInitMixin.java
+++ forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/ModMenuInitMixin.java
@@ -4,27 +4,26 @@ import com.google.common.collect.ImmutableMap;
 import com.terraformersmc.modmenu.ModMenu;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
 import java.util.HashMap;
 
-
+@Pseudo
 @Mixin(ModMenu.class)
 public class ModMenuInitMixin {
+	@Shadow(remap = false)
+	private static ImmutableMap<String, ConfigScreenFactory<?>> configScreenFactories;
 
-    @Shadow(remap = false)
-    private static ImmutableMap<String, ConfigScreenFactory<?>> configScreenFactories;
-
-    @Inject(method = "onInitializeClient", at = @At(value = "TAIL"), remap = false)
-    private void modify(CallbackInfo ci) {
-        var map = new HashMap<>(configScreenFactories);
-        String compat = "forgero-fabric-compat";
-        map.put("forgero", map.get(compat));
-        map.remove(compat);
-        configScreenFactories = ImmutableMap.copyOf(map);
-    }
+	@Inject(method = "onInitializeClient", at = @At(value = "TAIL"), remap = false)
+	private void modify(CallbackInfo ci) {
+		var map = new HashMap<>(configScreenFactories);
+		String compat = "forgero-fabric-compat";
+		map.put("forgero", map.get(compat));
+		map.remove(compat);
+		configScreenFactories = ImmutableMap.copyOf(map);
+	}
 }
