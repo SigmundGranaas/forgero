7a3ea98bdf379b802fec6f9a400291943edd7584
Sigmund Granaas Sandring <sigmundsgranaas@gmail.com>
2023-02-20 21:13:16 +0100

    Removed tests from compat

--- forgero-fabric-compat/src/test/java/com/sigmundgranaas/forgerofabric/gametest/EmptyTest.java
+++ forgero-fabric-compat/src/test/java/com/sigmundgranaas/forgerofabric/gametest/EmptyTest.java
@@ -6,7 +6,6 @@ import net.minecraft.test.TestContext;
 
 
 public class EmptyTest {
-
     @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE, batchId = "test_run", required = true)
     public void testRuns(TestContext context) {
         context.complete();
