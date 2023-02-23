f5605dbfbd948d9bb6fba5e7ebf9d3845cec775b
sigmundgranaas <sigmundsgranaas@gmail.com>
2023-02-23 17:41:42 +0100

    Fixing some translations

--- forgero-core/src/main/java/com/sigmundgranaas/forgero/core/condition/SimpleCondition.java
+++ forgero-core/src/main/java/com/sigmundgranaas/forgero/core/condition/SimpleCondition.java
@@ -1,6 +1,20 @@
 package com.sigmundgranaas.forgero.core.condition;
 
+import com.sigmundgranaas.forgero.core.property.Property;
 import com.sigmundgranaas.forgero.core.property.PropertyContainer;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class SimpleCondition implements PropertyContainer {
+    protected final List<Property> propertyList;
+
+    public SimpleCondition(List<Property> propertyList) {
+        this.propertyList = propertyList;
+    }
+
+    @Override
+    public @NotNull List<Property> getRootProperties() {
+        return propertyList;
+    }
 }
--- forgero-core/src/main/java/com/sigmundgranaas/forgero/core/state/composite/ConstructedTool.java
+++ forgero-core/src/main/java/com/sigmundgranaas/forgero/core/state/composite/ConstructedTool.java
@@ -1,8 +1,6 @@
 package com.sigmundgranaas.forgero.core.state.composite;
 
 import com.sigmundgranaas.forgero.core.condition.Conditional;
-import com.sigmundgranaas.forgero.core.condition.Conditions;
-import com.sigmundgranaas.forgero.core.condition.NamedCondition;
 import com.sigmundgranaas.forgero.core.configuration.ForgeroConfigurationLoader;
 import com.sigmundgranaas.forgero.core.property.Property;
 import com.sigmundgranaas.forgero.core.property.PropertyContainer;
@@ -22,6 +20,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
+
+import static com.sigmundgranaas.forgero.core.condition.Conditions.UNBREAKABLE;
 
 public class ConstructedTool extends ConstructedComposite implements SoulBindable, Conditional<ConstructedTool> {
     private final State head;
@@ -132,8 +132,8 @@ public class ConstructedTool extends ConstructedComposite implements SoulBindabl
     @Override
     public List<PropertyContainer> conditions() {
         List<PropertyContainer> customConditions = new ArrayList<>();
-        if (ForgeroConfigurationLoader.configuration.enableUnbreakableTools && conditions.stream().filter(condition -> condition instanceof NamedCondition).map(NamedCondition.class::cast).noneMatch(condition -> condition.name().equals(Conditions.UNBREAKABLE.name()))) {
-            customConditions.add(Conditions.UNBREAKABLE);
+        if (ForgeroConfigurationLoader.configuration.enableUnbreakableTools && conditions.stream().noneMatch(condition -> condition == UNBREAKABLE)) {
+            customConditions.add(UNBREAKABLE);
         }
         return Stream.of(conditions, customConditions).flatMap(List::stream).toList();
     }
