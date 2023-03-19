7e15ca9a5c5247da0307a4870d7fc12b143d008e
sigmund <sigmundsgranaas@gmail.com>
2023-03-19 22:04:08 +0100

    Basic condition testing

--- fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/customdata/EnchantmentVisitor.java
+++ fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/customdata/EnchantmentVisitor.java
@@ -1,17 +1,27 @@
 package com.sigmundgranaas.forgero.minecraft.common.customdata;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import com.sigmundgranaas.forgero.core.customdata.ClassBasedVisitor;
 import com.sigmundgranaas.forgero.core.customdata.DataContainer;
 import com.sigmundgranaas.forgero.core.customdata.DataVisitor;
+import com.sigmundgranaas.forgero.core.property.Target;
+
+import com.sigmundgranaas.forgero.core.property.TargetTypes;
+import com.sigmundgranaas.forgero.core.property.attribute.TypeTarget;
+import com.sigmundgranaas.forgero.core.resource.data.PropertyPojo;
+import com.sigmundgranaas.forgero.minecraft.common.conversion.StateConverter;
 import com.sigmundgranaas.forgero.minecraft.common.utils.RegistryUtils;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.registry.Registry;
+
+import javax.annotation.Nullable;
 
 /**
  * A {@link DataVisitor} for {@link EnchantmentData}.
@@ -54,6 +64,8 @@ public class EnchantmentVisitor extends ClassBasedVisitor<EnchantmentVisitor.Enc
 	public static class EnchantmentData {
 		private String id;
 		private int level;
+		@Nullable
+		private PropertyPojo.Condition condition;
 
 		/**
 		 * Embeds the enchantment into the {@link ItemStack} using native methods.
@@ -64,7 +76,17 @@ public class EnchantmentVisitor extends ClassBasedVisitor<EnchantmentVisitor.Enc
 			RegistryUtils.safeId(id())
 					.flatMap(id -> RegistryUtils.safeRegistryLookup(Registry.ENCHANTMENT, id))
 					.filter(enchant -> enchant.isAcceptableItem(stack))
+					.filter(enchant -> isConditionMet(stack))
 					.ifPresent(enchant -> stack.addEnchantment(enchant, level()));
+		}
+
+		public boolean isConditionMet(ItemStack stack) {
+			if(condition == null) {
+				return true;
+			}
+			Target target =	 new TypeTarget(Set.of(StateConverter.of(stack).orElseThrow().type().toString()));
+
+			return target.isApplicable(new HashSet<>(condition.tag), TargetTypes.TYPE);
 		}
 	}
 }
