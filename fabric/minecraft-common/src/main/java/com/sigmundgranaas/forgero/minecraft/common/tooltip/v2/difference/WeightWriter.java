75b0d5880ba92317549e0ee4627543dacd6947f7
Sigmund Granaas Sandring <46557903+SigmundGranaas@users.noreply.github.com>
2023-04-16 10:09:31 +0200

    Coilorized stats for attribute comparison (#570)
    * Arrows, customization and more colors per attribute

---
+++ fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/tooltip/v2/difference/WeightWriter.java
@@ -0,0 +1,37 @@
+package com.sigmundgranaas.forgero.minecraft.common.tooltip.v2.difference;
+
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+public class WeightWriter implements DifferenceWriter {
+	@Override
+	public Formatting getDifferenceFormatting(float difference) {
+		if (difference > 30) {
+			return Formatting.DARK_RED;
+		} else if (difference > 10) {
+			return Formatting.RED;
+		} else if (difference > 2) {
+			return Formatting.GOLD;
+		} else if (difference < -20) {
+			return Formatting.DARK_GREEN;
+		} else if (difference < -10) {
+			return Formatting.GREEN;
+		} else if (difference < -2) {
+			return Formatting.YELLOW;
+		} else {
+			return Formatting.WHITE;
+		}
+	}
+
+	@Override
+	public MutableText getDifferenceMarker(float difference) {
+		if (difference < 0) {
+			return Text.literal("↑");
+		} else if (difference > 0) {
+			return Text.literal("↓");
+		} else {
+			return Text.empty();
+		}
+	}
+}
