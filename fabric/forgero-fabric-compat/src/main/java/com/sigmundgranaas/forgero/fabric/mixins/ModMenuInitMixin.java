9f30b14331aefdc15112eb78cbb6a2eee4db0651
Steveplays28 <djspaargaren@outlook.com>
2023-04-21 18:47:27 +0200

    fix: Update dependencies to 1.19.4
    Disabled Patchouli compat since it hasn't been updated yet, this allows the game to load a world without crashing.
    Also removed unnecessary temporary files that should be excluded by the `.gitignore` file.

--- fabric/forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/ModMenuInitMixin.java
+++ fabric/forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/ModMenuInitMixin.java
@@ -15,16 +15,15 @@ import java.util.HashMap;
 
 @Mixin(ModMenu.class)
 public class ModMenuInitMixin {
-
-	@Shadow(remap = false)
-	private static ImmutableMap<String, ConfigScreenFactory<?>> configScreenFactories;
-
-	@Inject(method = "onInitializeClient", at = @At(value = "TAIL"), remap = false)
-	private void modify(CallbackInfo ci) {
-		var map = new HashMap<>(configScreenFactories);
-		String compat = "forgero-fabric-compat";
-		map.put("forgero", map.get(compat));
-		map.remove(compat);
-		configScreenFactories = ImmutableMap.copyOf(map);
-	}
+//	@Shadow(remap = false)
+//	private static ImmutableMap<String, ConfigScreenFactory<?>> configScreenFactories;
+//
+//	@Inject(method = "onInitializeClient", at = @At(value = "TAIL"), remap = false)
+//	private void modify(CallbackInfo ci) {
+//		var map = new HashMap<>(configScreenFactories);
+//		String compat = "forgero-fabric-compat";
+//		map.put("forgero", map.get(compat));
+//		map.remove(compat);
+//		configScreenFactories = ImmutableMap.copyOf(map);
+//	}
 }
