3d7ca66ced98e61ed93ca11ffadf27aedffefc52
sigmundgranaas <sigmundsgranaas@gmail.com>
2023-05-06 13:09:13 +0200

    Fixed Item groups

--- fabric/forgero-fabric-core/src/main/java/com/sigmundgranaas/forgero/fabric/item/StateToItemConverter.java
+++ fabric/forgero-fabric-core/src/main/java/com/sigmundgranaas/forgero/fabric/item/StateToItemConverter.java
@@ -12,8 +12,6 @@ import com.sigmundgranaas.forgero.core.util.match.Context;
 import com.sigmundgranaas.forgero.minecraft.common.item.DefaultStateItem;
 import com.sigmundgranaas.forgero.minecraft.common.item.ForgeroMaterial;
 import com.sigmundgranaas.forgero.minecraft.common.item.GemItem;
-import com.sigmundgranaas.forgero.minecraft.common.item.tool.*;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import com.sigmundgranaas.forgero.minecraft.common.item.tool.DynamicAxeItem;
 import com.sigmundgranaas.forgero.minecraft.common.item.tool.DynamicHoeItem;
 import com.sigmundgranaas.forgero.minecraft.common.item.tool.DynamicPickaxeItem;
@@ -24,9 +22,11 @@ import com.sigmundgranaas.forgero.minecraft.common.service.StateService;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ToolMaterials;
-import net.minecraft.registry.Registries;
 import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 
 public class StateToItemConverter {
 	private final StateProvider provider;
@@ -43,9 +43,13 @@ public class StateToItemConverter {
 		var context = Context.of();
 		var state = provider.get();
 		if (state.type().test(Type.of("SWORD"), context) || state.type().test(Type.of("TOOL"), context)) {
-			return createTool();
+			Item item = createTool();
+			ItemGroupEvents.modifyEntriesEvent(getItemGroup(provider.get())).register(entries -> entries.add(item));
+			return item;
 		} else if (state.type().test(Type.GEM)) {
-			return new GemItem(getItemSettings(state), state, StateService.INSTANCE);
+			Item item = new GemItem(getItemSettings(state), state, StateService.INSTANCE);
+			ItemGroupEvents.modifyEntriesEvent(getItemGroup(provider.get())).register(entries -> entries.add(item));
+			return item;
 		}
 		return defaultStateItem();
 	}
@@ -85,37 +89,37 @@ public class StateToItemConverter {
 		return new Identifier(provider.get().nameSpace(), provider.get().name());
 	}
 
-    private Item defaultStateItem() {
-        var item = new DefaultStateItem(new Item.Settings(), provider);
-        ItemGroupEvents.modifyEntriesEvent(getItemGroup(provider.get())).register(entries -> entries.add(item));
-        return item;
-    }
+	private Item defaultStateItem() {
+		var item = new DefaultStateItem(new Item.Settings(), provider);
+		ItemGroupEvents.modifyEntriesEvent(getItemGroup(provider.get())).register(entries -> entries.add(item));
+		return item;
+	}
 
-    public ItemGroup getItemGroup(State state) {
-        if (state.test(Type.TOOL)) {
-            return net.minecraft.item.ItemGroups.TOOLS;
-        } else if (state.test(Type.WEAPON)) {
-            return net.minecraft.item.ItemGroups.COMBAT;
-        } else if (state.test(Type.PART)) {
-            return ItemGroups.FORGERO_TOOL_PARTS;
-        } else if (state.test(Type.SCHEMATIC)) {
-            return ItemGroups.FORGERO_SCHEMATICS;
-        } else if (state.test(Type.TRINKET)) {
-            return ItemGroups.FORGERO_GEMS;
-        }
-        return net.minecraft.item.ItemGroups.INGREDIENTS;
-    }
+	public ItemGroup getItemGroup(State state) {
+		if (state.test(Type.TOOL)) {
+			return net.minecraft.item.ItemGroups.TOOLS;
+		} else if (state.test(Type.WEAPON)) {
+			return net.minecraft.item.ItemGroups.COMBAT;
+		} else if (state.test(Type.PART)) {
+			return ItemGroups.FORGERO_TOOL_PARTS;
+		} else if (state.test(Type.SCHEMATIC)) {
+			return ItemGroups.FORGERO_SCHEMATICS;
+		} else if (state.test(Type.TRINKET)) {
+			return ItemGroups.FORGERO_GEMS;
+		}
+		return net.minecraft.item.ItemGroups.INGREDIENTS;
+	}
 
 	private Item.Settings getItemSettings(State state) {
 		var settings = new Item.Settings();
 
-        if (state.name().contains("schematic")) {
-            settings.recipeRemainder(Registries.ITEM.get(new Identifier(state.identifier())));
-        }
+		if (state.name().contains("schematic")) {
+			settings.recipeRemainder(Registries.ITEM.get(new Identifier(state.identifier())));
+		}
 
-        if (state.name().contains("netherite")) {
-            settings.fireproof();
-        }
-        return settings;
-    }
+		if (state.name().contains("netherite")) {
+			settings.fireproof();
+		}
+		return settings;
+	}
 }
