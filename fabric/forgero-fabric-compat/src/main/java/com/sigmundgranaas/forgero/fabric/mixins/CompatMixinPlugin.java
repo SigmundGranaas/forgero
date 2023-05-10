4562da7bd1264678bc72da20d01e29349bf8befc
sigmundgranaas <sigmundsgranaas@gmail.com>
2023-05-11 01:08:05 +0200

    Updated loom, gradle and loader

--- fabric/forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/CompatMixinPlugin.java
+++ fabric/forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/CompatMixinPlugin.java
@@ -1,27 +1,27 @@
 package com.sigmundgranaas.forgero.fabric.mixins;
 
-import com.google.common.collect.ImmutableMap;
-import com.sigmundgranaas.forgero.fabric.ForgeroCompatInitializer;
-import org.objectweb.asm.tree.ClassNode;
-import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
-import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Supplier;
 
+import com.google.common.collect.ImmutableMap;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
 public class CompatMixinPlugin implements IMixinConfigPlugin {
 	private static final Supplier<Boolean> TRUE = () -> true;
 
 	private static final Map<String, Supplier<Boolean>> CONDITIONS = ImmutableMap.of(
-			"com.sigmundgranaas.forgero.fabric.mixins.BetterCombatWeaponRegistryMixin", () -> ForgeroCompatInitializer.bettercombat,
-			"com.sigmundgranaas.forgero.fabric.mixins.ModMenuInitMixin", () -> ForgeroCompatInitializer.modmenu
+			//"com.sigmundgranaas.forgero.fabric.mixins.BetterCombatWeaponRegistryMixin", () -> true,
+			//"com.sigmundgranaas.forgero.fabric.mixins.ModMenuInitMixin", () -> true
 	);
 
 	@Override
 	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-		return CONDITIONS.getOrDefault(mixinClassName, TRUE).get();
+		//return CONDITIONS.getOrDefault(mixinClassName, TRUE).get();
+		return true;
 	}
 
 	//Boilerplate
