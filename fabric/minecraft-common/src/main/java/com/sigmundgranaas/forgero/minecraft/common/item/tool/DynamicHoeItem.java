56f5f97192ae655f2fc93f0d012bd0fffb97fc6a
Sigmund Granaas Sandring <46557903+SigmundGranaas@users.noreply.github.com>
2023-05-13 12:29:38 +0200

    Fixed tool parts being treated as dynamic attribute tools (#608)

--- fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/item/tool/DynamicHoeItem.java
+++ fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/item/tool/DynamicHoeItem.java
@@ -1,9 +1,10 @@
 package com.sigmundgranaas.forgero.minecraft.common.item.tool;
 
+import java.util.List;
+
 import com.sigmundgranaas.forgero.core.state.State;
 import com.sigmundgranaas.forgero.core.state.StateProvider;
-import com.sigmundgranaas.forgero.minecraft.common.item.DynamicAttributeItem;
-import com.sigmundgranaas.forgero.minecraft.common.item.StateItem;
+import com.sigmundgranaas.forgero.minecraft.common.item.ToolStateItem;
 import com.sigmundgranaas.forgero.minecraft.common.tooltip.StateWriter;
 import com.sigmundgranaas.forgero.minecraft.common.tooltip.Writer;
 
@@ -14,9 +15,7 @@ import net.minecraft.item.ToolMaterial;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
 
-import java.util.List;
-
-public class DynamicHoeItem extends HoeItem implements DynamicAttributeItem, State, StateItem {
+public class DynamicHoeItem extends HoeItem implements ToolStateItem {
 	private final StateProvider DEFAULT;
 
 	public DynamicHoeItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings, StateProvider defaultState) {
@@ -26,7 +25,7 @@ public class DynamicHoeItem extends HoeItem implements DynamicAttributeItem, Sta
 
 	@Override
 	public int getItemBarStep(ItemStack stack) {
-		return StateItem.super.getItemBarStep(stack);
+		return ToolStateItem.super.getItemBarStep(stack);
 	}
 
 	public int getItemBarColor(ItemStack stack) {
