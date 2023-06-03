fd2a094d7c8eadaf893c22b0815e9165299f2764
sigmundgranaas <sigmundsgranaas@gmail.com>
2023-06-03 19:13:54 +0200

    Functional slot placement and rendering with tooltip cycling

--- fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/block/upgradestation/UpgradeStationScreen.java
+++ fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/block/upgradestation/UpgradeStationScreen.java
@@ -1,28 +1,71 @@
 package com.sigmundgranaas.forgero.minecraft.common.block.upgradestation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.sigmundgranaas.forgero.minecraft.common.service.StateService;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.util.math.Vector2f;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.tag.TagKey;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec3f;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
+
 
 public class UpgradeStationScreen extends HandledScreen<UpgradeStationScreenHandler> {
-	private static final Identifier TEXTURE = new Identifier("forgero", "textures/gui/container/assembly_table_ui.png");
-
-	private final StateService service;
+	private static final Identifier TEXTURE = new Identifier("forgero", "textures/gui/container/upgrade_table_ui.png");
+	private static final Identifier textureId = new Identifier("minecraft", "textures/gui/advancements/widgets.png");
 	private final ItemRenderer itemRenderer;
+	private int tickCounter;
 
 	public UpgradeStationScreen(UpgradeStationScreenHandler handler, PlayerInventory inventory, Text title) {
 		super(handler, inventory, Text.translatable("block.forgero.assembly_station"));
 		this.itemRenderer = MinecraftClient.getInstance().getItemRenderer();
-		this.service = StateService.INSTANCE;
 	}
+
+
+	public void renderCustomTooltip(MatrixStack matrices, List<Text> lines, int mouseX, int mouseY) {
+		for (UpgradeStationScreenHandler.PositionedSlot slot : this.handler.slots.stream().filter(UpgradeStationScreenHandler.PositionedSlot.class::isInstance).map(UpgradeStationScreenHandler.PositionedSlot.class::cast).toList()) {
+			if (isPointWithinBounds(slot.x, slot.y, 16, 16, mouseX, mouseY) && !slot.hasStack()) {
+				lines.add(Text.literal("This slot accepts...")); // Here you need to add the description based on the slot
+				renderCycledSlot(slot, lines, mouseX, mouseY);
+			}
+		}
+	}
+
+	public void renderCycledSlot(UpgradeStationScreenHandler.PositionedSlot slot, List<Text> tooltip, int mouseX, int mouseY) {
+		if (slot.getSlot() != null) {
+			com.sigmundgranaas.forgero.core.state.Slot slot1 = slot.getSlot();
+
+			Identifier id = new Identifier("forgero", slot1.typeName().toLowerCase());
+
+			TagKey<Item> key = TagKey.of(Registry.ITEM_KEY, id);
+			List<RegistryEntry<Item>> entries = new ArrayList<>();
+			// List of items that this slot accepts
+			Registry.ITEM.iterateEntries(key).forEach(entries::add);
+			List<Item> acceptedItems = entries.stream().map(RegistryEntry::value).toList();
+
+			// Select an item to display based on the current game tick
+			Item itemToShow = acceptedItems.get((this.tickCounter / 40) % acceptedItems.size());
+
+			this.itemRenderer.renderInGuiWithOverrides(itemToShow.getDefaultStack(), mouseX, mouseY);
+		}
+	}
+
 
 	@Override
 	protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
@@ -32,14 +75,91 @@ public class UpgradeStationScreen extends HandledScreen<UpgradeStationScreenHand
 		int x = (width - backgroundWidth) / 2;
 		int y = (height - backgroundHeight) / 2;
 		drawTexture(matrices, x, y, 0, 0, backgroundWidth, backgroundHeight);
+
+		for (Slot slot : handler.slots) {
+			// Render your slots here
+			if (slot instanceof UpgradeStationScreenHandler.PositionedSlot slot1) {
+				this.drawSlot(matrices, slot1);
+			}
+		}
+
+	}
+
+	private void drawSlot(MatrixStack matrices, UpgradeStationScreenHandler.PositionedSlot slot) {
+		// You'll want to render your slot here. I'll use default slot rendering as an example,
+		// but you might have your own custom slot rendering.
+
+		int x = slot.x - 1;
+		int y = slot.y - 1;
+
+		int centerX = (this.width - this.backgroundWidth) / 2;
+		int centerY = (this.height - this.backgroundHeight) / 2;
+		RenderSystem.setShaderTexture(0, TEXTURE);
+
+		this.drawTexture(matrices, centerX + x, centerY + y, 194, 55, 18, 18);
+
+		if (!slot.hasStack()) {
+			ItemStack stack = new ItemStack(Items.IRON_INGOT);
+			Sprite sprite = itemRenderer.getModels().getModel(stack).getParticleSprite();
+
+			//RenderSystem.setShaderTexture(0, sprite.getAtlas().getId());
+			//drawSprite(matrices, centerX + x + 1, centerY + y + 1, this.getZOffset(), 16, 16, sprite);
+		}
 	}
 
 	@Override
 	public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
 		renderBackground(matrices);
 		super.render(matrices, mouseX, mouseY, delta);
+		this.tickCounter++;
+
+		MinecraftClient.getInstance().getTextureManager().bindTexture(textureId);
+
+		for (Slot slot : handler.slots) {
+			if (!(slot instanceof UpgradeStationScreenHandler.PositionedSlot positionedSlot)) {
+				continue;
+			}
+			for (Slot otherSlot : handler.slots) {
+				if (!(otherSlot instanceof UpgradeStationScreenHandler.PositionedSlot otherPositionedSlot)) {
+					continue;
+				}
+
+				// Draw a line if the slots are on subsequent layers
+				if (Math.abs(positionedSlot.yPosition - otherPositionedSlot.yPosition) == handler.verticalSpacing
+						&& Math.abs(positionedSlot.xPosition - otherPositionedSlot.xPosition) < handler.horizontalSpacing) {
+
+					Vector2f startPos = new Vector2f(positionedSlot.xPosition + 8, positionedSlot.yPosition + 8);
+					Vector2f endPos = new Vector2f(otherPositionedSlot.xPosition + 8, otherPositionedSlot.yPosition + 8);
+					float distance = (float) Math.sqrt(Math.pow(endPos.getX() - startPos.getX(), 2) + Math.pow(endPos.getY() - startPos.getY(), 2));
+					float angle = (float) Math.atan2(endPos.getY() - startPos.getY(), endPos.getX() - startPos.getX());
+
+					for (float i = 0; i < 1; i += 1.0f / distance) {
+						Vector2f pos = new Vector2f(
+								startPos.getX() + (endPos.getX() - startPos.getX()) * i,
+								startPos.getY() + (endPos.getY() - startPos.getY()) * i
+						);
+
+						matrices.push();
+						matrices.translate(pos.getX(), pos.getY(), 0);
+						matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(angle * (180F / (float) Math.PI)));
+
+						DrawableHelper.drawTexture(
+								matrices,
+								0, 0,
+								0, 0,
+								16, 16,
+								256, 256
+						);
+
+						matrices.pop();
+					}
+				}
+			}
+		}
 		drawMouseoverTooltip(matrices, mouseX, mouseY);
+		renderCustomTooltip(matrices, new ArrayList<>(), mouseX, mouseY);
 	}
+
 
 	@Override
 	protected void init() {
--- fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/block/upgradestation/UpgradeStationScreenHandler.java
+++ fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/block/upgradestation/UpgradeStationScreenHandler.java
@@ -1,6 +1,16 @@
 package com.sigmundgranaas.forgero.minecraft.common.block.upgradestation;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
 import com.sigmundgranaas.forgero.core.state.Composite;
+import com.sigmundgranaas.forgero.core.state.State;
+import com.sigmundgranaas.forgero.core.state.composite.ConstructedState;
+import com.sigmundgranaas.forgero.core.type.Type;
 import com.sigmundgranaas.forgero.minecraft.common.service.StateService;
 
 import net.minecraft.entity.player.PlayerEntity;
@@ -15,6 +25,7 @@ import net.minecraft.screen.slot.Slot;
 
 public class UpgradeStationScreenHandler extends ScreenHandler {
 
+	private static final int SLOT_SIZE = 18;
 	public static ScreenHandler dummyHandler = new ScreenHandler(ScreenHandlerType.CRAFTING, 0) {
 		@Override
 		public ItemStack transferSlot(PlayerEntity player, int index) {
@@ -26,11 +37,14 @@ public class UpgradeStationScreenHandler extends ScreenHandler {
 			return true;
 		}
 	};
+	public final int verticalSpacing = 25;   // adjust this value as needed
+	public final int horizontalSpacing = 24; // adjust this value as needed
 	private final SimpleInventory inventory;
 	private final ScreenHandlerContext context;
 	private final PlayerEntity player;
 	private final CompositeSlot compositeSlot;
 	private final StateService service;
+	private final List<Slot> slotList;
 
 	//This constructor gets called on the client when the server wants it to open the screenHandler,
 	//The client will call the other constructor with an empty Inventory and the screenHandler will automatically
@@ -51,8 +65,9 @@ public class UpgradeStationScreenHandler extends ScreenHandler {
 		inventory.onOpen(playerInventory.player);
 		SimpleInventory compositeInventory = new SimpleInventory(1);
 		compositeInventory.addListener(this::onCompositeSlotChanged);
-		this.compositeSlot = new CompositeSlot(compositeInventory, 0, 34, 34);
+		this.compositeSlot = new CompositeSlot(compositeInventory, 0, 80, 10);
 		this.service = StateService.INSTANCE;
+		this.slotList = new ArrayList<>();
 		//This will place the slot in the correct locations for a 3x3 Grid. The slots exist on both server and client!
 		//This will not render the background of the slots however, this is the Screens job
 		int m;
@@ -75,24 +90,83 @@ public class UpgradeStationScreenHandler extends ScreenHandler {
 
 	public void onCompositeSlotChanged(Inventory compositeInventory) {
 		var component = service.convert(compositeInventory.getStack(0));
-		if (component.isPresent() && component.get() instanceof Composite composite) {
-			for (com.sigmundgranaas.forgero.core.state.Slot slot : composite.slots()) {
-				// If the upgrade is interactable, add a Slot here
-				if (slot.filled()) {
-					var inventory = new SimpleInventory(1);
-					inventory.setStack(0, service.convert(slot.get().get()).get());
-					addSlot(new Slot(inventory, 2, compositeSlot.x, compositeSlot.y - 20));
-					//handler.addSlot(new Slot(inventory, upgrade.getSlotIndex(), x + indent, y + 20));
-					//itemRenderer.renderInGuiWithOverrides(service.convert(slot.get().get()).get(), x + indent, y + 20);
-				} else {
-					// Render non-interactable upgrade item icon here
-					//itemRenderer.renderInGuiWithOverrides(upgrade.getItemStack(), x + indent, y + 20);
-				}
+		if (component.isPresent()) {
+			this.slotList.forEach(this.slots::remove);
+			slotList.clear();
+			ToolTree toolTree = new ToolTree(component.get());
+			toolTree.buildTree();
+			placeSlots(toolTree.getRoot(), 80, 35, 2);
+		} else {
+			this.slotList.forEach(this.slots::remove);
+			slotList.clear();
+		}
+	}
 
-				// Draw any child upgrades
+	private void placeSlots(TreeNode node, int parentOffsetX, int offsetY, int slotSpacing) {
+		// Calculate total number of slots for the node, including children's slots
+		int totalSlots = node.getLeafCount();
+		// Calculate total slot width and spacing width
+		int totalSlotWidth = totalSlots * SLOT_SIZE;
+		int totalSpacingWidth = (totalSlots - 1) * slotSpacing;
+		int totalWidth = totalSlotWidth + totalSpacingWidth;
+		// Calculate starting offset X
+		int startOffsetX = parentOffsetX - totalWidth / 2;
 
+		int currentWidth = 0;
+
+		for (TreeNode child : node.getChildren()) {
+			int childSlots = child.getLeafCount();
+			int childSlotWidth = childSlots * SLOT_SIZE;
+			int childSpacingWidth = (childSlots - 1) * slotSpacing;
+			int childWidth = childSlotWidth + childSpacingWidth;
+			int slotOffsetX = startOffsetX + currentWidth + childWidth / 2;
+
+			var inventory = new SimpleInventory(1);
+			var slot = new PositionedSlot(inventory, 0, slotOffsetX, offsetY, child.slot);
+			slotList.add(slot);
+			addSlot(slot);
+			slot.insertStack(service.convert(child.getState()).orElse(ItemStack.EMPTY));
+
+			placeSlots(child, slotOffsetX, offsetY + verticalSpacing, slotSpacing);
+
+			currentWidth += childWidth + slotSpacing;
+		}
+	}
+
+
+	private int calculateSubtreeWidthHelper(State state, Map<State, Integer> widthMap) {
+		if (widthMap.containsKey(state)) {
+			return widthMap.get(state);
+		}
+
+		int width = 0;
+
+		if (state instanceof ConstructedState construct) {
+			for (State constructed : construct.parts()) {
+				width += calculateSubtreeWidthHelper(constructed, widthMap);
 			}
 		}
+
+		if (state instanceof Composite composite) {
+			for (com.sigmundgranaas.forgero.core.state.Slot upgradeSlot : composite.slots()) {
+				// Width is added for every slot, whether filled or not
+				width += 1;
+
+				// If the slot is filled, recursively calculate the width of the upgrade
+				if (upgradeSlot.filled()) {
+					width += calculateSubtreeWidthHelper(upgradeSlot.get().get(), widthMap);
+				}
+			}
+		}
+
+		widthMap.put(state, width);
+		return width;
+	}
+
+	private Map<State, Integer> calculateSubtreeWidth(State root) {
+		Map<State, Integer> widthMap = new HashMap<>();
+		calculateSubtreeWidthHelper(root, widthMap);
+		return widthMap;
 	}
 
 	@Override
@@ -102,6 +176,7 @@ public class UpgradeStationScreenHandler extends ScreenHandler {
 			this.dropInventory(player, this.inventory);
 		});
 	}
+
 
 	@Override
 	public boolean canUse(PlayerEntity player) {
@@ -135,11 +210,128 @@ public class UpgradeStationScreenHandler extends ScreenHandler {
 
 
 	private static class CompositeSlot extends Slot {
-
-
 		public CompositeSlot(Inventory inventory, int index, int x, int y) {
 			super(inventory, index, x, y);
 
+		}
+	}
+
+	public static class PositionedSlot extends Slot {
+		public final int xPosition;
+		public final int yPosition;
+		@Nullable
+		public final com.sigmundgranaas.forgero.core.state.Slot slot;
+
+		public PositionedSlot(Inventory inventory, int index, int xPosition, int yPosition, @Nullable com.sigmundgranaas.forgero.core.state.Slot slot) {
+			super(inventory, index, xPosition, yPosition);
+			this.xPosition = xPosition;
+			this.yPosition = yPosition;
+			this.slot = slot;
+		}
+
+		public @Nullable com.sigmundgranaas.forgero.core.state.Slot getSlot() {
+			return slot;
+		}
+	}
+
+	public static class TreeNode {
+		private final State state;
+		private final List<TreeNode> children;
+		@Nullable
+		private final com.sigmundgranaas.forgero.core.state.Slot slot;
+		private TreeNode parent;
+
+		public TreeNode(State state, @Nullable com.sigmundgranaas.forgero.core.state.Slot slot) {
+			this.state = state;
+			this.children = new ArrayList<>();
+			this.slot = slot;
+		}
+
+		public int getLeafCount() {
+			if (children.isEmpty()) {
+				return 1;
+			} else {
+				return children.stream().mapToInt(TreeNode::getLeafCount).sum();
+			}
+		}
+
+
+		public void addChild(TreeNode child) {
+			child.parent = this;
+			this.children.add(child);
+		}
+
+		public State getState() {
+			return this.state;
+		}
+
+		public List<TreeNode> getChildren() {
+			return this.children;
+		}
+
+		public TreeNode getParent() {
+			return this.parent;
+		}
+
+		public State getParentPart() {
+			return this.parent.getState();
+		}
+	}
+
+	public static class EmptyState implements State {
+		@Override
+		public String name() {
+			return "empty";
+		}
+
+		@Override
+		public String nameSpace() {
+			return "forgero";
+		}
+
+		@Override
+		public Type type() {
+			return Type.of("EMPTY");
+		}
+	}
+
+	// Empty state representing an unfilled slot
+
+	public class ToolTree {
+		private final TreeNode root;
+
+		public ToolTree(State rootState) {
+			this.root = new TreeNode(rootState, null);
+		}
+
+		public TreeNode getRoot() {
+			return this.root;
+		}
+
+		// Method to build the tree structure recursively from the given root state
+		public void buildTree() {
+			this.buildTreeHelper(this.root);
+		}
+
+		private void buildTreeHelper(TreeNode node) {
+			State state = node.getState();
+			if (state instanceof ConstructedState construct) {
+				for (State constructed : construct.parts()) {
+					if (constructed instanceof Composite) {
+						TreeNode childNode = new TreeNode(constructed, null);
+						node.addChild(childNode);
+						this.buildTreeHelper(childNode);
+					}
+				}
+			}
+			if (state instanceof Composite composite) {
+				for (com.sigmundgranaas.forgero.core.state.Slot upgradeSlot : composite.slots()) {
+					State childState = upgradeSlot.filled() ? upgradeSlot.get().get() : new EmptyState();
+					TreeNode childNode = new TreeNode(childState, upgradeSlot);
+					node.addChild(childNode);
+					this.buildTreeHelper(childNode);
+				}
+			}
 		}
 	}
 
