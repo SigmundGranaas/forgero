1e63274e80070d84e7dd7da1d88de99673a40446
Sigmund Granaas Sandring <46557903+SigmundGranaas@users.noreply.github.com>
2023-08-22 16:03:55 +0200

    Support any tag in packs (#707)
    * Added new dependency options for packages and resources
    * Added bronze, silver and steel to common materials
    * Added mythic metals common tags dynamically

--- fabric/forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/ForgeroCompatInitializer.java
+++ fabric/forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/ForgeroCompatInitializer.java
@@ -3,6 +3,7 @@ package com.sigmundgranaas.forgero.fabric;
 import java.util.function.Supplier;
 
 import com.sigmundgranaas.forgero.fabric.api.entrypoint.ForgeroInitializedEntryPoint;
+import com.sigmundgranaas.forgero.fabric.mythicmetals.MythicMetalsCommons;
 import com.sigmundgranaas.forgero.fabric.patchouli.BookDropOnAdvancement;
 import com.sigmundgranaas.forgero.fabric.patchouli.GuideBookGenerator;
 import com.sigmundgranaas.forgero.fabric.toolstats.ToolStatTagGenerator;
@@ -15,9 +16,12 @@ public class ForgeroCompatInitializer implements ForgeroInitializedEntryPoint {
 	public static final Supplier<Boolean> patchouli;
 	public static final Supplier<Boolean> modmenu;
 	public static final Supplier<Boolean> bettercombat;
+	public static final Supplier<Boolean> mythicmetals;
+
 
 	static {
 		toolstats = () -> isModLoaded("toolstats");
+		mythicmetals = () -> isModLoaded("mythicmetals");
 		patchouli = () -> isModLoaded("patchouli");
 		modmenu = () -> isModLoaded("modmenu");
 		bettercombat = () -> isModLoaded("bettercombat");
@@ -36,6 +40,10 @@ public class ForgeroCompatInitializer implements ForgeroInitializedEntryPoint {
 		if (patchouli.get()) {
 			BookDropOnAdvancement.registerBookDrop();
 			GuideBookGenerator.registerGuideBookRecipes();
+		}
+
+		if (mythicmetals.get()) {
+			MythicMetalsCommons.generateTags();
 		}
 	}
 }
