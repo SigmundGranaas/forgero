f4372b31db71b412b3c27921c4fe9188fdb328d0
sigmund <sigmundsgranaas@gmail.com>
2023-10-14 13:35:20 +0200

    * Reimplemented the feature writer to create tooltips from basePredicateFeatures

--- fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/tooltip/v2/section/FeatureSectionWriter.java
+++ fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/tooltip/v2/section/FeatureSectionWriter.java
@@ -1,11 +1,16 @@
 package com.sigmundgranaas.forgero.minecraft.common.tooltip.v2.section;
 
-import java.util.Collections;
+import static com.sigmundgranaas.forgero.core.util.Identifiers.EMPTY_IDENTIFIER;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import com.sigmundgranaas.forgero.core.property.PropertyContainer;
+import com.sigmundgranaas.forgero.core.property.v2.feature.BasePredicateFeature;
+import com.sigmundgranaas.forgero.core.property.v2.feature.Feature;
 import com.sigmundgranaas.forgero.minecraft.common.tooltip.v2.TooltipConfiguration;
+import com.sigmundgranaas.forgero.minecraft.common.tooltip.v2.WriterHelper;
 
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.text.Text;
@@ -34,7 +39,7 @@ public class FeatureSectionWriter extends SectionWriter {
 
 	@Override
 	public boolean shouldWrite() {
-		return false;
+		return !container.stream().features().toList().isEmpty();
 	}
 
 	@Override
@@ -54,8 +59,48 @@ public class FeatureSectionWriter extends SectionWriter {
 
 	@Override
 	public List<Text> entries() {
-		return Collections.emptyList();
-
+		return container.stream()
+				.features()
+				.map(feature -> BaseFeatureWriter.of(feature, new WriterHelper(configuration)))
+				.flatMap(Optional::stream)
+				.map(BaseFeatureWriter::write)
+				.flatMap(List::stream)
+				.toList();
 	}
 
+	public static class BaseFeatureWriter {
+		private final BasePredicateFeature feature;
+		private final WriterHelper helper;
+
+		private BaseFeatureWriter(BasePredicateFeature feature, WriterHelper helper) {
+			this.feature = feature;
+			this.helper = helper;
+		}
+
+		public static Optional<BaseFeatureWriter> of(Feature feature, WriterHelper helper) {
+			if (feature instanceof BasePredicateFeature base) {
+				if (!base.title().equals(EMPTY_IDENTIFIER)) {
+					return Optional.of(new BaseFeatureWriter(base, helper));
+				}
+			}
+			return Optional.empty();
+		}
+
+		public List<Text> write() {
+			List<Text> entries = new ArrayList<>();
+
+			Text title = helper.writeBase()
+					.append(Text.translatable(feature.title()))
+					.formatted(helper.neutral());
+			entries.add(title);
+			if (!feature.description().equals(EMPTY_IDENTIFIER)) {
+				Text description = helper.writeBase()
+						.append(" ")
+						.append(Text.translatable(feature.description()))
+						.formatted(helper.base());
+				entries.add(description);
+			}
+			return entries;
+		}
+	}
 }
--- forgero-core/src/main/java/com/sigmundgranaas/forgero/core/property/v2/feature/BasePredicateFeature.java
+++ forgero-core/src/main/java/com/sigmundgranaas/forgero/core/property/v2/feature/BasePredicateFeature.java
@@ -1,29 +1,26 @@
 package com.sigmundgranaas.forgero.core.property.v2.feature;
 
-import javax.annotation.Nullable;
-
 import com.sigmundgranaas.forgero.core.util.match.MatchContext;
 import com.sigmundgranaas.forgero.core.util.match.Matchable;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 
+@Getter
+@Accessors(fluent = true)
 public abstract class BasePredicateFeature implements Feature, Matchable {
-	protected String id;
-	protected String type;
-	protected Matchable predicate;
-	@Nullable
-	protected String title;
-	@Nullable
-	protected String description;
-
-	public BasePredicateFeature(String id, String type, Matchable predicate) {
-		this.id = id;
-		this.type = type;
-		this.predicate = predicate;
-	}
+	protected final String id;
+	protected final String type;
+	protected final Matchable predicate;
+	protected final String title;
+	protected final String description;
 
 	public BasePredicateFeature(BasePredicateData data) {
 		this.id = data.id();
 		this.type = data.type();
 		this.predicate = data.predicate();
+		this.title = data.title();
+		this.description = data.description();
+
 	}
 
 	@Override
