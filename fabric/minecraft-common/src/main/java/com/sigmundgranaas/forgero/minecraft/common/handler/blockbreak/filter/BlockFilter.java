f335ca3d8911c8b35a149b0f16f968c5ef24b2b5
sigmund <sigmundsgranaas@gmail.com>
2023-10-26 08:25:41 +0200

    * Implemented basic filters for vein mining as well

--- fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/handler/blockbreak/filter/BlockFilter.java
+++ fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/handler/blockbreak/filter/BlockFilter.java
@@ -1,14 +1,49 @@
 package com.sigmundgranaas.forgero.minecraft.common.handler.blockbreak.filter;
 
+import static com.sigmundgranaas.forgero.minecraft.common.handler.blockbreak.filter.SameBlockFilter.Key;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gson.JsonElement;
+import com.sigmundgranaas.forgero.core.property.v2.feature.ClassKey;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 
 @FunctionalInterface
 public interface BlockFilter {
+	ClassKey<BlockFilter> KEY = new ClassKey<>("forgero:filter", BlockFilter.class);
+
+	BlockFilter DEFAULT = (source, current, root) -> true;
+
+
 	static BlockFilter fromJson(JsonElement json) {
-		return ((entity, currentPos, root) -> true);
+		if (json.isJsonPrimitive() && json.getAsJsonPrimitive().isString()) {
+			String type = json.getAsJsonPrimitive().getAsString();
+			return fromString(type);
+		}
+		if (json.isJsonArray()) {
+			List<BlockFilter> filters = new ArrayList<>();
+			json.getAsJsonArray().forEach(element -> filters.add(fromJson(element)));
+			return new FilterWrapper(filters);
+		}
+
+		if (json.isJsonObject()) {
+			var object = json.getAsJsonObject();
+			if (object.has("type")) {
+				var type = object.get("type");
+				return fromJson(type);
+			}
+		}
+		return BlockFilter.DEFAULT;
+	}
+
+	static BlockFilter fromString(String type) {
+		if (type.equals(Key)) {
+			return new SameBlockFilter();
+		}
+		return BlockFilter.DEFAULT;
 	}
 
 	boolean filter(Entity entity, BlockPos currentPos, BlockPos root);
