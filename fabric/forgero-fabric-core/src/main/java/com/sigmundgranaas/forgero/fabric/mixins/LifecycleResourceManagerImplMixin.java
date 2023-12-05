4b1e063daf36accc6845c890bf1ec7ad18673f12
sigmund <sigmundsgranaas@gmail.com>
2023-12-05 18:02:15 +0100

    * Fully implemented animated Palettes and Templates

--- fabric/forgero-fabric-core/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/LifecycleResourceManagerImplMixin.java
+++ fabric/forgero-fabric-core/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/LifecycleResourceManagerImplMixin.java
@@ -1,27 +1,27 @@
 package com.sigmundgranaas.forgero.fabric.mixins;
 
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.sigmundgranaas.forgero.core.texture.V2.FileLoader;
 import com.sigmundgranaas.forgero.core.texture.V2.TextureGenerator;
 import com.sigmundgranaas.forgero.fabric.client.ForgeroClient;
 import com.sigmundgranaas.forgero.fabric.resources.ResourceLoadedFileService;
-
-import net.minecraft.resource.LifecycledResourceManagerImpl;
-import net.minecraft.resource.Resource;
-import net.minecraft.util.Identifier;
-
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Optional;
+import net.minecraft.resource.LifecycledResourceManagerImpl;
+import net.minecraft.resource.Resource;
+import net.minecraft.resource.metadata.ResourceMetadata;
+import net.minecraft.util.Identifier;
 
 @Mixin(LifecycledResourceManagerImpl.class)
 public abstract class LifecycleResourceManagerImplMixin {
-
-	@Shadow
-	public abstract Optional<Resource> getResource(Identifier identifier);
 
 	@Inject(method = "getResource", at = @At("RETURN"), cancellable = true)
 	public void getResource(Identifier id, CallbackInfoReturnable<Optional<Resource>> cir) {
@@ -32,11 +32,18 @@ public abstract class LifecycleResourceManagerImplMixin {
 				FileLoader loader = new ResourceLoadedFileService();
 				var texture = TextureGenerator.getInstance(loader, ForgeroClient.PALETTE_REMAP).getTexture(ForgeroClient.TEXTURES.get(textureId));
 				if (texture.isPresent()) {
-					Resource resource = new Resource(id.getNamespace(), texture.get()::getStream);
-
+					var metadata = TextureGenerator.getInstance(loader, ForgeroClient.PALETTE_REMAP).getMetadata(ForgeroClient.TEXTURES.get(textureId), ".mcmeta");
+					Resource resource;
+					resource = metadata
+							.map(object -> new Resource(id.getNamespace(), texture.get()::getStream, convert(object)))
+							.orElseGet(() -> new Resource(id.getNamespace(), texture.get()::getStream));
 					cir.setReturnValue(Optional.of(resource));
 				}
 			}
 		}
+	}
+
+	private Resource.InputSupplier<ResourceMetadata> convert(JsonObject object) {
+		return () -> ResourceMetadata.create(new ByteArrayInputStream(new Gson().toJson(object).getBytes(StandardCharsets.UTF_8)));
 	}
 }
