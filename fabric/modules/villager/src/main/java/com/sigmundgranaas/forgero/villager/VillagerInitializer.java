c6737bc4899fd469b64ab6c657cdf3946753fca1
sigmund <sigmundsgranaas@gmail.com>
2023-12-26 17:12:02 +0100

    * Creating the structure for the villager module

---
+++ fabric/modules/villager/src/main/java/com/sigmundgranaas/forgero/villager/VillagerInitializer.java
@@ -0,0 +1,12 @@
+package com.sigmundgranaas.forgero.villager;
+
+import net.fabricmc.api.ModInitializer;
+
+public class VillagerInitializer implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+}
