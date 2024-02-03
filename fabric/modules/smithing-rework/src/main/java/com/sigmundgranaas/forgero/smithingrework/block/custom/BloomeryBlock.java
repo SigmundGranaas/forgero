adaa588e5bcefb7e13f846cdb1f154ab175f25fe
jorisdebeer <jorisvkeulen@gmail.com>
2024-02-03 19:32:03 +0100

    Added crucible and tongs placeholder

---
+++ fabric/modules/smithing-rework/src/main/java/com/sigmundgranaas/forgero/smithingrework/block/custom/BloomeryBlock.java
@@ -0,0 +1,9 @@
+package com.sigmundgranaas.forgero.smithingrework.block.custom;
+
+import net.minecraft.block.FurnaceBlock;
+
+public class BloomeryBlock extends FurnaceBlock {
+	public BloomeryBlock(Settings settings) {
+		super(settings);
+	}
+}
--- fabric/modules/smithing-rework/src/main/java/com/sigmundgranaas/forgero/smithingrework/item/ModItems.java
+++ fabric/modules/smithing-rework/src/main/java/com/sigmundgranaas/forgero/smithingrework/item/ModItems.java
@@ -2,6 +2,8 @@ package com.sigmundgranaas.forgero.smithingrework.item;
 
 import com.sigmundgranaas.forgero.core.Forgero;
 
+import com.sigmundgranaas.forgero.smithingrework.item.custom.CrucibleItem;
+
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
@@ -15,12 +17,16 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-	public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
-	public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings()));
+	public static final Item SMITHING_HAMMER = registerItem("smithing_hammer", new Item(new FabricItemSettings()));
+	public static final Item TONGS = registerItem("tongs", new Item(new FabricItemSettings()));
+	public static final Item CLAY_CRUCIBLE = registerItem("clay_crucible", new Item(new FabricItemSettings()));
+	public static final Item CRUCIBLE = registerItem("crucible", new CrucibleItem(new FabricItemSettings()));
 
 	private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
-		entries.add(RUBY);
-		entries.add(RAW_RUBY);
+		entries.add(SMITHING_HAMMER);
+		entries.add(TONGS);
+		entries.add(CRUCIBLE);
+		entries.add(CLAY_CRUCIBLE);
 	}
 
 	private static Item registerItem(String name, Item item) {
