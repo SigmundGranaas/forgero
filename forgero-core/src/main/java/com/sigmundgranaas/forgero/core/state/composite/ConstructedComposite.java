abea4440de9bb9207f1e67f1d5d80dbfa8fb70f6
Sigmund Granaas Sandring <46557903+SigmundGranaas@users.noreply.github.com>
2024-03-11 22:29:05 +0100

    [Patch]: Fixed properties being tested before being applied (#866)

--- forgero-core/src/main/java/com/sigmundgranaas/forgero/core/state/composite/ConstructedComposite.java
+++ forgero-core/src/main/java/com/sigmundgranaas/forgero/core/state/composite/ConstructedComposite.java
@@ -77,7 +77,7 @@ public class ConstructedComposite extends BaseComposite implements ConstructedSt
 		var props = new ArrayList<>(super.compositeProperties(target, context));
 
 		var partProps = parts().stream()
-				.flatMap(part -> part.applyProperty(target, context).stream().map(part::applySource))
+				.flatMap(part -> part.getProperties().stream().map(part::applySource))
 				.toList();
 
 		props.addAll(propertyProcessor.process(partProps, target, context));
