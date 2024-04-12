5fd75e94b585de8d1d9f550dbe83114f898a80c9
sigmund <sigmundsgranaas@gmail.com>
2024-04-12 08:35:30 +0200

    * Added an itemstack check for the DefaultedModelStrategy

--- fabric/forgero-fabric-core/src/main/java/com/sigmundgranaas/forgero/fabric/client/model/baked/strategy/DefaultModelStrategy.java
+++ fabric/forgero-fabric-core/src/main/java/com/sigmundgranaas/forgero/fabric/client/model/baked/strategy/DefaultModelStrategy.java
@@ -1,10 +1,14 @@
 package com.sigmundgranaas.forgero.fabric.client.model.baked.strategy;
 
+import static com.sigmundgranaas.forgero.minecraft.common.match.MinecraftContextKeys.STACK;
+
 import java.util.Optional;
 
 import com.sigmundgranaas.forgero.core.state.State;
 import com.sigmundgranaas.forgero.core.util.match.MatchContext;
 import com.sigmundgranaas.forgero.minecraft.common.client.model.BakedModelResult;
+
+import net.minecraft.item.ItemStack;
 
 public class DefaultModelStrategy implements ModelStrategy {
 	private final BakedModelResult result;
@@ -17,7 +21,10 @@ public class DefaultModelStrategy implements ModelStrategy {
 
 	@Override
 	public Optional<BakedModelResult> getModel(State state, MatchContext context) {
-		if (code == state.hashCode()) {
+		Optional<ItemStack> stack = context.get(STACK);
+		if (stack.isPresent() && !stack.get().hasNbt()) {
+			return Optional.of(result);
+		} else if (code == state.hashCode()) {
 			return Optional.of(result);
 		} else {
 			return Optional.empty();
