1c6eeaaab2b2a994167817edff9b1481d11c769f
sigmund <sigmundsgranaas@gmail.com>
2024-04-29 21:09:22 +0200

    * Added two modifications and a slot to mastercrafted heads

--- forgero-core/src/main/java/com/sigmundgranaas/forgero/core/state/upgrade/slot/FilledSlot.java
+++ forgero-core/src/main/java/com/sigmundgranaas/forgero/core/state/upgrade/slot/FilledSlot.java
@@ -42,7 +42,7 @@ public class FilledSlot extends AbstractTypedSlot {
 	}
 
 	private List<Property> filterProperties(Matchable target, MatchContext context) {
-		var properties = upgrade.applyProperty(target, context).stream()
+		var properties = upgrade.getRootProperties(target, context).stream()
 				.map(upgrade::applySource)
 				.toList();
 		return new UpgradePropertyProcessor(categories).process(properties, target, context.put(SLOT_CONTEXT_KEY, this));
