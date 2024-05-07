5fb2931af3d37f79c99f358408f32243621f5566
Sigmund Granaas Sandring <46557903+SigmundGranaas@users.noreply.github.com>
2024-05-07 18:56:50 +0200

    [Patch]: Fix reach issues (#915)
    Closes #913 
    Closes #880

--- fabric/forgero-fabric-core/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/ClientPlayerInteractionManagerReachMixin.java
+++ fabric/forgero-fabric-core/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/ClientPlayerInteractionManagerReachMixin.java
@@ -3,16 +3,17 @@ package com.sigmundgranaas.forgero.fabric.mixins;
 import com.sigmundgranaas.forgero.core.property.v2.ComputedAttribute;
 import com.sigmundgranaas.forgero.core.type.Type;
 import com.sigmundgranaas.forgero.minecraft.common.service.StateService;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.network.ClientPlayerInteractionManager;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.item.ItemStack;
 
 @Mixin(ClientPlayerInteractionManager.class)
 public class ClientPlayerInteractionManagerReachMixin {
@@ -26,15 +27,13 @@ public class ClientPlayerInteractionManagerReachMixin {
 
 		if (player != null) {
 			ItemStack mainHandStack = player.getMainHandStack();
-
 			if (!mainHandStack.isEmpty()) {
 				StateService.INSTANCE.convert(mainHandStack)
 						.filter(state -> state.test(Type.HOLDABLE))
 						.map(container -> ComputedAttribute.of(container, "forgero:reach"))
 						.map(ComputedAttribute::asFloat)
-						.filter(value -> value > 0)
-						.map(reach -> player.isCreative() ? reach + 0.5 : reach)
-						.map(Double::floatValue)
+						.map((reach) -> reach - 4.5f)
+						.map(reach -> reach + cir.getReturnValue())
 						.ifPresent(cir::setReturnValue);
 			}
 		}
