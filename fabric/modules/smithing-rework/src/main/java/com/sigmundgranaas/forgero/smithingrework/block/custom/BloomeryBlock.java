d6e9a52195d3c3a92c2e995a74ce684d7a633718
jorisdebeer <jorisvkeulen@gmail.com>
2024-07-08 23:29:21 +0200

    HIHIHIH AHAHAHAH HIHIHHIH HAHAHAHA

--- fabric/modules/smithing-rework/src/main/java/com/sigmundgranaas/forgero/smithingrework/block/custom/BloomeryBlock.java
+++ fabric/modules/smithing-rework/src/main/java/com/sigmundgranaas/forgero/smithingrework/block/custom/BloomeryBlock.java
@@ -5,6 +5,7 @@ import com.sigmundgranaas.forgero.smithingrework.block.entity.BloomeryBlockEntit
 import com.sigmundgranaas.forgero.smithingrework.block.entity.ModBlockEntities;
 import com.sigmundgranaas.forgero.smithingrework.block.entity.SmithingAnvilBlockEntity;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.AbstractFurnaceBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
@@ -12,7 +13,9 @@ import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.CampfireBlock;
 import net.minecraft.block.FurnaceBlock;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
@@ -22,8 +25,12 @@ import net.minecraft.block.entity.FurnaceBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
@@ -35,6 +42,7 @@ import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockRenderView;
@@ -43,16 +51,26 @@ import net.minecraft.world.World;
 
 import org.jetbrains.annotations.Nullable;
 
-public class BloomeryBlock extends FurnaceBlock {
-	public String type;
+public class BloomeryBlock extends AbstractFurnaceBlock {
+	private static final VoxelShape SHAPE = Block.createCuboidShape(0, 0, 0, 16, 14, 16);
+	public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
 
-	public BloomeryBlock(MapColor colour, float strength, String type) {
-		super(Settings.copy(Blocks.FURNACE).strength(strength).mapColor(colour));
-		this.type = type;
+	public String type;
+	private final boolean emitsParticles;
+
+	public BloomeryBlock(boolean emitsParticles, AbstractBlock.Settings settings) {
+		super(settings);
+		this.emitsParticles = emitsParticles;
+		this.setDefaultState((BlockState)((BlockState)((BlockState)this.stateManager.getDefaultState()).with(FACING, Direction.NORTH)).with(LIT, false));
 	}
-	public BloomeryBlock(MapColor colour, BlockSoundGroup sound, float strength, String type) {
-		super(Settings.copy(Blocks.FURNACE).mapColor(colour).strength(strength).sounds(sound));
-		this.type = type;
+
+	@Override
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		return (BlockState)this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+	}
+
+	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		return SHAPE;
 	}
 
 	@Override
@@ -68,6 +86,35 @@ public class BloomeryBlock extends FurnaceBlock {
 			player.openHandledScreen((NamedScreenHandlerFactory)blockEntity);
 			player.incrementStat(Stats.INTERACT_WITH_FURNACE);
 		}
+	}
+
+	public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+		if (!state.get(LIT).booleanValue()) {
+			return;
+		}
+		if (random.nextInt(10) == 0) {
+			world.playSound((double) pos.getX() + 0.5, (double) pos.getY() + 0.5, (double) pos.getZ() + 0.5, SoundEvents.BLOCK_CAMPFIRE_CRACKLE, SoundCategory.BLOCKS, 0.5f + random.nextFloat(), random.nextFloat() * 0.7f + 0.6f, false);
+		}
+		if (random.nextInt(5) == 0) {
+			for (int i = 0; i < random.nextInt(1) + 1; ++i) {
+				BloomeryBlock.spawnSmokeParticle(world, pos, state.get(BloomeryBlock.LIT), true);
+			}
+		}
+		if (this.emitsParticles && random.nextInt(5) == 0) {
+			for (int i = 0; i < random.nextInt(1) + 1; ++i) {
+				world.addParticle(ParticleTypes.LAVA, (double) pos.getX() + 0.5, (double) pos.getY() + 1.2, (double) pos.getZ() + 0.5, random.nextFloat() / 2.0f, 5.0E-5, random.nextFloat() / 2.0f);
+			}
+		}
+	}
+
+	public static void spawnSmokeParticle(World world, BlockPos pos, boolean isSignal, boolean lotsOfSmoke) {
+		Random random = world.getRandom();
+		DefaultParticleType defaultParticleType = isSignal ? ParticleTypes.CAMPFIRE_SIGNAL_SMOKE : ParticleTypes.CAMPFIRE_COSY_SMOKE;
+		world.addImportantParticle(defaultParticleType, true, (double)pos.getX() + 0.5 + random.nextDouble() / 3.0 * (double)(random.nextBoolean() ? 1 : -1), (double) pos.getY() + 1.2 + random.nextDouble() + random.nextDouble(), (double)pos.getZ() + 0.5 + random.nextDouble() / 3.0 * (double)(random.nextBoolean() ? 1 : -1), 0.0, 0.07, 0.0);
+		if (lotsOfSmoke) {
+			world.addParticle(ParticleTypes.SMOKE,(double) pos.getX() + 0.5, (double) pos.getY() + 1.2, (double) pos.getZ() + 0.5, 0.0, 0.005, 0.0);
+		}
+
 	}
 
 	@Nullable
--- fabric/modules/smithing-rework/src/main/java/com/sigmundgranaas/forgero/smithingrework/block/entity/BloomeryBlockEntity.java
+++ fabric/modules/smithing-rework/src/main/java/com/sigmundgranaas/forgero/smithingrework/block/entity/BloomeryBlockEntity.java
@@ -2,20 +2,41 @@ package com.sigmundgranaas.forgero.smithingrework.block.entity;
 
 import com.sigmundgranaas.forgero.smithingrework.block.custom.BloomeryBlock;
 
+import net.minecraft.block.AbstractFurnaceBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.CampfireBlock;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.block.entity.CampfireBlockEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeType;
 import net.minecraft.screen.FurnaceScreenHandler;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.text.Text;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+
+import org.jetbrains.annotations.Nullable;
 
 public class BloomeryBlockEntity extends AbstractFurnaceBlockEntity {
 	public BloomeryBlockEntity(BlockPos pos, BlockState state) {
 		super(ModBlockEntities.BLOOMERY_BLOCK, pos, state, RecipeType.SMELTING);
 	}
+
+	public static final BooleanProperty LIT = Properties.LIT;
 
 	@Override
 	protected Text getContainerName() {
@@ -27,9 +48,21 @@ public class BloomeryBlockEntity extends AbstractFurnaceBlockEntity {
 		return new FurnaceScreenHandler(syncId, playerInventory, this, this.propertyDelegate);
 	}
 
+	public static void clientTick(World world, BlockPos pos, BlockState state, CampfireBlockEntity campfire) {
+		int i;
+		Random random = world.random;
+		if (random.nextFloat() < 0.11f) {
+			for (i = 0; i < random.nextInt(2) + 2; ++i) {
+				CampfireBlock.spawnSmokeParticle(world, pos, state.get(CampfireBlock.SIGNAL_FIRE), false);
+			}
+		}
+	}
+
+
 	protected BloomeryBlock getBlock() {
 		return (BloomeryBlock) getCachedState().getBlock();
 	}
+
 }
 
 
