493c99ff16584b27d4f5476806049fe294792a8b
Steveplays28 <djspaargaren@outlook.com>
2024-07-27 16:28:39 +0200

    refactor: Fix warnings in `AssemblyStationScreen`

--- fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/block/assemblystation/AssemblyStationScreen.java
+++ fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/block/assemblystation/AssemblyStationScreen.java
@@ -5,18 +5,19 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
+import org.jetbrains.annotations.NotNull;
+
 public class AssemblyStationScreen extends HandledScreen<AssemblyStationScreenHandler> {
 	private static final Identifier TEXTURE = new Identifier("forgero", "textures/gui/container/assembly_table_ui.png");
 
+	@SuppressWarnings("unused")
 	public AssemblyStationScreen(AssemblyStationScreenHandler handler, PlayerInventory inventory, Text title) {
 		super(handler, inventory, Text.translatable("block.forgero.assembly_station"));
 	}
-
 
 	@Override
 	protected void init() {
@@ -25,9 +26,8 @@ public class AssemblyStationScreen extends HandledScreen<AssemblyStationScreenHa
 		titleX = (backgroundWidth - textRenderer.getWidth(title)) / 2;
 	}
 
-
 	@Override
-	protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
+	protected void drawBackground(@NotNull DrawContext context, float delta, int mouseX, int mouseY) {
 		RenderSystem.setShader(GameRenderer::getPositionTexProgram);
 		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
 		RenderSystem.setShaderTexture(0, TEXTURE);
@@ -37,9 +37,9 @@ public class AssemblyStationScreen extends HandledScreen<AssemblyStationScreenHa
 	}
 
 	@Override
-	public void render(DrawContext matrices, int mouseX, int mouseY, float delta) {
-		renderBackground(matrices);
-		super.render(matrices, mouseX, mouseY, delta);
-		drawMouseoverTooltip(matrices, mouseX, mouseY);
+	public void render(@NotNull DrawContext context, int mouseX, int mouseY, float delta) {
+		renderBackground(context);
+		super.render(context, mouseX, mouseY, delta);
+		drawMouseoverTooltip(context, mouseX, mouseY);
 	}
 }
