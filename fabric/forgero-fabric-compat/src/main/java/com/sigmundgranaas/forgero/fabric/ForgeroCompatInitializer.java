e66a9f88c90de11f71537e28a4c42d145f4c3983
sigmund <sigmundsgranaas@gmail.com>
2024-07-27 09:26:24 +0200

    * Removed patchouli code references

--- fabric/forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/ForgeroCompatInitializer.java
+++ fabric/forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/ForgeroCompatInitializer.java
@@ -1,31 +1,28 @@
 package com.sigmundgranaas.forgero.fabric;
 
+import java.util.function.Supplier;
+
 import com.sigmundgranaas.forgero.fabric.api.entrypoint.ForgeroInitializedEntryPoint;
 import com.sigmundgranaas.forgero.fabric.mythicmetals.MythicMetalsCommons;
 import com.sigmundgranaas.forgero.fabric.patchouli.BookDropOnAdvancement;
-import com.sigmundgranaas.forgero.fabric.patchouli.GuideBookGenerator;
 import com.sigmundgranaas.forgero.fabric.toolstats.ToolStatTagGenerator;
 import com.sigmundgranaas.forgero.minecraft.common.service.StateService;
-import net.fabricmc.loader.api.FabricLoader;
 
-import java.util.function.Supplier;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class ForgeroCompatInitializer implements ForgeroInitializedEntryPoint {
 	public static final Supplier<Boolean> toolstats;
-	public static final Supplier<Boolean> patchouli;
 	public static final Supplier<Boolean> bettercombat;
 	public static final Supplier<Boolean> mythicmetals;
 	public static final Supplier<Boolean> yacl;
 	public static final Supplier<Boolean> emi;
 	public static final Supplier<Boolean> modonomicon;
 
-
 	static {
 		modonomicon = () -> isModLoaded("modonomicon");
 		toolstats = () -> isModLoaded("toolstats");
 		emi = () -> isModLoaded("emi");
 		mythicmetals = () -> isModLoaded("mythicmetals");
-		patchouli = () -> isModLoaded("patchouli");
 		bettercombat = () -> isModLoaded("bettercombat");
 		yacl = () -> isModLoaded("yet-another-config-lib") || isModLoaded("yet_another_config_lib_v3");
 	}
