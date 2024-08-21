1f7a8a37542ff8393e39cd4804f696a1913a017e
Sigmund Granaas Sandring <46557903+SigmundGranaas@users.noreply.github.com>
2024-08-21 11:29:55 +0200

    [Patch]: Created a new setup for registering common tags (#1049)
    Changes how common item tags are registered to make each mod create a
    new dynamic resource pack for the tags. This solves the issue of mods
    overriding tags added by other mods in the same resource packs.
    
    Example:
    ```
    public class ModernIndustrialization extends CommonTagGenerator {
    	protected ModernIndustrialization() {
    		super("modernindustrialization","modern_industrialization");
    	}
    
    	@Override
    	public void addTags() {
    		registerCommonItemTag("bronze_ingot");
    		registerCommonItemTag("steel_ingot");
    		registerCommonItemTag("aluminum_ingot");
    		registerCommonItemTag("lead_ingot");
    		registerCommonItemTag("invar_ingot");
    		registerCommonItemTag("nickel_ingot");
    		registerCommonItemTag("silver_ingot");
    		registerCommonItemTag("titanium_ingot");
    		registerCommonItemTag("electrum_ingot");
    		registerCommonItemTag("chromium_ingot");
    		registerCommonItemTag("platinum_ingot");
    		registerCommonItemTag("iridium_ingot");
    		registerCommonItemTag("tungsten_ingot");
    	}
    }
    ```
    
    Registering it in `CommonTags`
    ```
    	private static final List<Supplier<CommonTagGenerator>> TAGS = List.of(
    			...
    			ModernIndustrialization::new
    	);
    ```

--- fabric/forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/tags/Meadow.java
+++ fabric/forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/tags/Meadow.java
@@ -1,14 +1,13 @@
 package com.sigmundgranaas.forgero.fabric.tags;
 
-import static com.sigmundgranaas.forgero.fabric.resources.ARRPGenerator.RESOURCE_PACK;
+public class Meadow extends CommonTagGenerator {
+	protected Meadow() {
+		super("meadow");
+	}
 
-import net.devtech.arrp.json.tags.JTag;
-
-import net.minecraft.util.Identifier;
-
-public class Meadow {
-	public static void generateTags() {
-		RESOURCE_PACK.addTag(new Identifier("c", "items/limestone"), JTag.tag().add(new Identifier("meadow:limestone")));
-		RESOURCE_PACK.addTag(new Identifier("c", "items/pine_planks"), JTag.tag().add(new Identifier("meadow:pine_planks")));
+	@Override
+	public void addTags() {
+		registerCommonItemTag("limestone");
+		registerCommonItemTag("pine_planks");
 	}
 }
