fe1d5032999de787c7f0d196fe79108e6edc34ff
sigmund <sigmundsgranaas@gmail.com>
2024-08-27 15:56:11 +0200

    * Experimenting with in world upgrades

--- fabric/forgero-fabric-core/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/ItemDescriptionInjector.java
+++ fabric/forgero-fabric-core/src/main/java/com/sigmundgranaas/forgero/fabric/mixins/ItemDescriptionInjector.java
@@ -3,10 +3,23 @@ package com.sigmundgranaas.forgero.fabric.mixins;
 import static com.sigmundgranaas.forgero.minecraft.common.tooltip.Writer.writeModifierSection;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.sigmundgranaas.forgero.core.state.Composite;
+import com.sigmundgranaas.forgero.core.state.SimpleState;
+import com.sigmundgranaas.forgero.core.state.State;
 import com.sigmundgranaas.forgero.minecraft.common.item.StateItem;
 import com.sigmundgranaas.forgero.minecraft.common.service.StateService;
 import com.sigmundgranaas.forgero.minecraft.common.tooltip.v2.DefaultWriter;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+
+import net.minecraft.util.UseAction;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,8 +33,65 @@ import net.minecraft.registry.Registries;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
 
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 @Mixin(Item.class)
 public class ItemDescriptionInjector {
+
+	@Inject(at = @At("TAIL"), method = "use", cancellable = true)
+	public void forgero$dynamicUse(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+		Item item = (Item) (Object) this;
+		ItemStack tool = user.getStackInHand(Hand.MAIN_HAND);
+
+		String id = Registries.ITEM.getId(item).toString();
+		Optional<State> state = StateService.INSTANCE.find(id);
+		Optional<State> toolState = StateService.INSTANCE.convert(tool);
+
+		if (state.isPresent() && hand == Hand.OFF_HAND && toolState.isPresent() && toolState.get() instanceof Composite upgradeable) {
+			user.setCurrentHand(hand);
+			cir.setReturnValue(TypedActionResult.consume(user.getStackInHand(hand)));
+		}
+	}
+
+	@Inject(at = @At("HEAD"), method = "finishUsing")
+	public void forgero$dynamicUseUpgrade(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) {
+		Item item = (Item) (Object) this;
+		ItemStack tool = user.getStackInHand(Hand.MAIN_HAND);
+
+		String id = Registries.ITEM.getId(item).toString();
+		Optional<State> state = StateService.INSTANCE.find(id);
+		Optional<State> toolState = StateService.INSTANCE.convert(tool);
+
+		if (state.isPresent() && stack.equals(user.getStackInHand(Hand.OFF_HAND)) && toolState.isPresent() && toolState.get() instanceof Composite upgradeable) {
+				Composite composite = upgradeable.upgrade(state.get());
+				user.setStackInHand(Hand.MAIN_HAND, StateService.INSTANCE.convert(composite).get());
+				user.getStackInHand(Hand.OFF_HAND).decrement(1);
+			}
+	}
+
+	@Inject(at = @At("HEAD"), method = "getMaxUseTime", cancellable = true)
+	public void forgero$dynamicUseUpgrade(ItemStack stack, CallbackInfoReturnable<Integer> cir) {
+		Item item = (Item) (Object) this;
+
+		String id = Registries.ITEM.getId(item).toString();
+		Optional<State> state = StateService.INSTANCE.find(id);
+
+		if (state.isPresent() && !(state.get() instanceof Composite)) {
+			cir.setReturnValue(32);
+		}
+	}
+
+	@Inject(at = @At("HEAD"), method = "getUseAction", cancellable = true)
+	public void forgero$dynamicUseAction(ItemStack stack, CallbackInfoReturnable<UseAction> cir) {
+		Item item = stack.getItem();
+
+		String id = Registries.ITEM.getId(item).toString();
+		Optional<State> state = StateService.INSTANCE.find(id);
+
+		if (state.isPresent() && !(state.get() instanceof Composite)) {
+			cir.setReturnValue(UseAction.EAT);
+		}
+	}
 
 	@SuppressWarnings("DataFlowIssue")
 	@Inject(at = @At("HEAD"), method = "appendTooltip")
