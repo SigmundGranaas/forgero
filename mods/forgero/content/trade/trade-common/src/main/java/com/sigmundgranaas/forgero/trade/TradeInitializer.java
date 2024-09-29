a9985968ca5b0d4321ad16e7246f848e26f54d2b
Steveplays28 <djspaargaren@outlook.com>
2024-09-29 13:24:13 +0200

    refactor: Abstract all submods

---
+++ mods/forgero/content/trade/trade-common/src/main/java/com/sigmundgranaas/forgero/trade/TradeInitializer.java
@@ -0,0 +1,11 @@
+package com.sigmundgranaas.forgero.trade;
+
+import com.sigmundgranaas.forgero.fabric.api.entrypoint.ForgeroPreInitializationEntryPoint;
+import com.sigmundgranaas.forgero.trademodule.util.ForgeroCustomTrades;
+
+public class TradeInitializer implements ForgeroPreInitializationEntryPoint {
+	@Override
+	public void onPreInitialization() {
+		ForgeroCustomTrades.registerCustomTrades();
+	}
+}
