4722f621a725859f1960514d9c08a316fda36747
sigmund <sigmundsgranaas@gmail.com>
2024-12-02 15:59:11 +0100

    * removed traces of patchouli

--- fabric/forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/ForgeroCompatClient.java
+++ fabric/forgero-fabric-compat/src/main/java/com/sigmundgranaas/forgero/fabric/ForgeroCompatClient.java
@@ -1,12 +1,8 @@
 package com.sigmundgranaas.forgero.fabric;
 
-import com.sigmundgranaas.forgero.fabric.patchouli.GemUpgradeRecipePage;
-import com.sigmundgranaas.forgero.fabric.patchouli.StateCraftingRecipePage;
-import com.sigmundgranaas.forgero.fabric.patchouli.StateUpgradeRecipePage;
 import com.sigmundgranaas.forgero.fabric.tag.BlockTagCompatRegistration;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.util.Identifier;
 
@@ -17,12 +13,6 @@ public class ForgeroCompatClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		if (modonomicon.get()) {
 			ModelLoadingPlugin.register(ctx -> ctx.addModels(new ModelIdentifier(new Identifier("forgero:guidebook"), "inventory")));
-		}
-
-		if (FabricLoader.getInstance().isModLoaded("patchouli")) {
-			GemUpgradeRecipePage.register();
-			StateCraftingRecipePage.register();
-			StateUpgradeRecipePage.register();
 		}
 
 		BlockTagCompatRegistration.register();
