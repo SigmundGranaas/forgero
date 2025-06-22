554c414f893a97714346a50b0fcb3b6f3fadf130
JorisDeBeer <jorisvkeulen@gmail.com>
2025-06-22 15:26:00 +0200

    Different way same result.

--- fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/toolhandler/block/selector/BlockSelectionUtils.java
+++ fabric/minecraft-common/src/main/java/com/sigmundgranaas/forgero/minecraft/common/toolhandler/block/selector/BlockSelectionUtils.java
@@ -15,18 +15,23 @@ public class BlockSelectionUtils {
 
 	/**
 	 * @param blockPos the root position to find blocks around
-	 * @return A set of all the blocks that are directly adjacent to the given block position (sharing a face)
+	 * @return A set of all the blocks that are adjacent to the given block position, including diagonals
 	 */
 	public static Set<BlockPos> getBlockPositionsAround(BlockPos blockPos) {
 		Set<BlockPos> offsetBlockPositions = new HashSet<>();
 
-		// Add only the 6 directly adjacent blocks (sharing a face)
-		offsetBlockPositions.add(blockPos.add(1, 0, 0));  // East
-		offsetBlockPositions.add(blockPos.add(-1, 0, 0)); // West
-		offsetBlockPositions.add(blockPos.add(0, 1, 0));  // Up
-		offsetBlockPositions.add(blockPos.add(0, -1, 0)); // Down
-		offsetBlockPositions.add(blockPos.add(0, 0, 1));  // South
-		offsetBlockPositions.add(blockPos.add(0, 0, -1)); // North
+		// Get all 26 blocks surrounding the center block (3x3x3 cube excluding center)
+		for (int x = -1; x <= 1; x++) {
+			for (int y = -1; y <= 1; y++) {
+				for (int z = -1; z <= 1; z++) {
+					// Skip the center block
+					if (x == 0 && y == 0 && z == 0) {
+						continue;
+					}
+					offsetBlockPositions.add(blockPos.add(x, y, z));
+				}
+			}
+		}
 
 		return offsetBlockPositions;
 	}
