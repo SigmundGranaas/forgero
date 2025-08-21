8a15667c629e2bebb99b89cbcd08fb35e59e6f15
JorisDeBeer <jorisvkeulen@gmail.com>
2025-08-22 01:51:50 +0200

    Some runTimeModel bugfixes

--- fabric/modules/smithing/src/main/java/com/sigmundgranaas/forgero/smithing/util/RuntimeModelUtil.java
+++ fabric/modules/smithing/src/main/java/com/sigmundgranaas/forgero/smithing/util/RuntimeModelUtil.java
@@ -3,6 +3,7 @@ package com.sigmundgranaas.forgero.smithing.util;
 import java.awt.image.BufferedImage;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Optional;
 
 import javax.imageio.ImageIO;
 
@@ -13,7 +14,9 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
+import net.minecraft.resource.Resource;
 
 public class RuntimeModelUtil {
     private static final Logger LOGGER = LogManager.getLogger("ForgeroRuntimeModelUtil");
@@ -26,9 +29,13 @@ public class RuntimeModelUtil {
      * @return BufferedImage of the texture, or null if not found or error
      */
     public static BufferedImage getFirstQuadTextureImage(ItemStack itemStack, MinecraftClient client) {
+        if (itemStack.isEmpty() || itemStack.getItem() == Items.AIR) {
+            return null;
+        }
         try {
             BakedModel model = client.getItemRenderer().getModel(itemStack, null, null, 0);
-            List<?> quads = model.getQuads(null, null, client.world.getRandom());
+            net.minecraft.util.math.random.Random random = (client.world != null) ? client.world.getRandom() : net.minecraft.util.math.random.Random.create();
+            List<?> quads = model.getQuads(null, null, random);
             Sprite textureSprite = null;
             for (Object quadObj : quads) {
                 var quad = (net.minecraft.client.render.model.BakedQuad) quadObj;
@@ -42,12 +49,16 @@ public class RuntimeModelUtil {
             }
             Identifier spriteId = textureSprite.getContents().getId();
             Identifier resourceId = new Identifier(spriteId.getNamespace(), "textures/" + spriteId.getPath() + ".png");
-            LOGGER.debug("[RuntimeModelUtil] Using texture resource: {} for itemStack: {}", resourceId, itemStack);
-            try (InputStream stream = client.getResourceManager().getResource(resourceId).get().getInputStream()) {
-                return ImageIO.read(stream);
+            Optional<Resource> resourceOpt = client.getResourceManager().getResource(resourceId);
+            if (resourceOpt.isPresent()) {
+                try (InputStream stream = resourceOpt.get().getInputStream()) {
+                    return ImageIO.read(stream);
+                }
+            } else {
+                return null;
             }
         } catch (Exception e) {
-            LOGGER.error("[RuntimeModelUtil] Error extracting quad texture image: ", e);
+
             return null;
         }
     }
